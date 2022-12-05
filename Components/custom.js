d3b8d6cb20ae57b70620cf8dc16b4878a57ed07f
johnwalker721@gmail.com <johnwalker721@gmail.com>
2022-12-05 18:57:08 -0500

    added navaigation

---
+++ Components/custom.js
@@ -0,0 +1,32 @@
+const NavTemplate = document.createElement('template');
+NavTemplate.innerHTML = `
+<div class="header">
+    <slot name="logo"></slot>
+    <slot name="links">
+        <a href="#">Home</a>
+        <a href="#">Login</a>
+    </slot>
+    </div>
+    <style>
+        .header {
+            background-color: #333;
+            color: #fff;
+            padding: 10px;
+        }
+        .header a {
+            color: #fff;
+            text-decoration: none;
+            padding: 10px;
+        }
+    </style>
+`;
+
+class NavBar extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
+        <slot></slot>
+        `;
+    }
+}
