334b5b745745b9ef2a97dd16b9dea36c1350992f
Samuel Kahura <kahurasamuel@gmail.com>
2022-12-06 13:41:17 -0500

    signup and login done

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,7 +2,44 @@
 
 "use strict";
 
+const $q = (s) => document.querySelector(s);
+
 const loginForm = document.querySelector("#login");
+const loginDiv = document.querySelector("#loginDiv");
+const registratonForm = $q("#registration");
+const registratonDiv = $q("#registrationDiv");
+const fullName = $q("#regFullName");
+const userName = $q("#regUsername");
+const password = $q("#regPassword");
+const signUPForm = $q("#signUPForm");
+const loginFormSwitch = $q("#loginForm")
+
+function registerNewUser(event) {
+    event.preventDefault();
+
+    const bodyData = {
+        username: userName.value,
+        fullName: fullName.value,
+        password: password.value
+    }
+
+    fetch("https://microbloglite.herokuapp.com/api/users", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(bodyData)
+    }).then((response) => response.json)
+        .then((user) => {
+            console.log(user);
+            window.location.href = "./index.html";
+            sessionStorage.message = `Welcome ${userName.value}`
+        })
+        .catch((err) => {
+            console.log(err);
+
+        })
+
+}
+
 
 loginForm.onsubmit = function (event) {
     // Prevent the form from refreshing the page,
@@ -22,3 +59,34 @@ loginForm.onsubmit = function (event) {
     // Time to actually process the login using the function from auth.js!
     login(loginData);
 };
+
+
+function displaySignUpPage() {
+    if (registratonDiv.style.display === 'none') {
+        registratonDiv.style.display = 'block'
+        loginDiv.style.display = 'none'
+    } else {
+        registratonDiv.style.display = 'none'
+        // loginForm.style.display = 'block'
+    }
+}
+
+registratonDiv.style.display = 'none'
+
+function displayLoginPage() {
+    if (loginDiv.style.display === 'none') {
+        loginDiv.style.display = 'block'
+        registratonDiv.style.display = 'none'
+
+    } else {
+        loginDiv.style.display = 'none'
+    }
+}
+
+
+window.onload = () => {
+    registratonForm.onsubmit = registerNewUser;
+    signUPForm.onclick = displaySignUpPage
+    loginFormSwitch.onclick = displayLoginPage
+
+}
