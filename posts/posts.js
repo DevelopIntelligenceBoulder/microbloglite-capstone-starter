b6b30120dc709c8c953fc456d573f95e63f4fdf3
Eric Harris <ericoldie@gmail.com>
2022-12-08 12:57:13 -0500

    like fetch api working

--- posts/posts.js
+++ posts/posts.js
@@ -6,12 +6,123 @@ const $q = (s) => document.querySelector(s);
 const logoutButton = $q("#logoutButton");
 const cardSection = $q("#card-section");
 
+const loginData = getLoginData();
+const postAPI = "https://microbloglite.herokuapp.com/api/posts/";
+
 function getLoginData() {
   return JSON.parse(window.localStorage.getItem("login-data")) || {};
 }
 
 
+function displayProfilePost() {
+  const options = {
+    method: "GET",
+    headers: {
+      // This header is how we authenticate our user with the
+      // server for any API requests which require the user
+      // to be logged-in in order to have access.
+      // In the API docs, these endpoints display a lock icon.
+      Authorization: `Bearer ${loginData.token}`,
+    },
+  };
 
+  fetch(postAPI, options)
+    .then((response) => response.json())
+    .then((posts) => {
+      posts.forEach((post) => {
+          buildPostCard(cardSection, post);
+        
+      });
+    });
+ }
+
+
+function buildPostCard(section, data) {
+  //create col div for BS
+  const colDiv = document.createElement("div");
+  colDiv.className = "col";
+
+  //created the card
+  const cardDiv = document.createElement("div");
+
+  cardDiv.className = "card p-2";
+
+  //put the colDiv on the card-section div then put cardDiv inside colDiv
+  section.appendChild(colDiv);
+  colDiv.appendChild(cardDiv);
+
+  //create card title for username
+  const cardTitle = document.createElement("h5");
+
+  cardTitle.className = "card-title";
+  cardTitle.innerText = data.username;
+
+  cardTitle.className = "card-title text-center";
+  cardTitle.innerText = `@${data.username}`;
+
+  //create card div text for the card body
+
+  const cardTextPara = document.createElement("p");
+  cardTextPara.className = "card-text";
+  cardTextPara.innerText = data.text;
+
+  //create the .card-body div to plant the card-text div
+  // create div to hold btn-group div with other bs-utilities
+  const dFlexDiv = document.createElement("div");
+  dFlexDiv.className = "d-flex justify-content-between align-items-center";
+  //create btn-group div to carry the btns
+  const btnGroupDiv = document.createElement("div");
+  btnGroupDiv.className = "btn-group";
+
+  //create btns and timeposted to put inside the btnGroupDiv
+  const likeBtn = document.createElement("button");
+  likeBtn.className = "btn btn-sm btn-outline-secondary";
+  likeBtn.innerText = "Like";
+  function likePost(event) {
+    event.preventDefault();
+    let id = data._id;
+    const bodyData = {
+      postId: data._id
+    };
+
+    
+    fetch("https://microbloglite.herokuapp.com/api/likes", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${loginData.token}`,
+      },
+      body: JSON.stringify(bodyData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        sessionStorage.message = "Succesfully Liked.";
+        likeBtn.className = "btn btn-sm btn-success";
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  likeBtn.onclick = likePost;
+
+  const postTime = document.createElement("small");
+  postTime.className = "text-muted";
+  let timeCreated = new Date(data.createdAt);
+  postTime.innerText = `${timeCreated.toLocaleString()}`;
+
+  btnGroupDiv.appendChild(likeBtn);
+
+  dFlexDiv.append(btnGroupDiv, postTime);
+  //create the .card-body div to plant inside the card-text div
+  const divCardBody = document.createElement("div");
+  divCardBody.className = "card-body";
+
+  cardDiv.appendChild(divCardBody);
+  divCardBody.append(cardTitle, cardTextPara);
+
+  cardDiv.append(cardTitle, divCardBody);
+  divCardBody.append(cardTextPara, dFlexDiv);
+}
 
 
 const userName = $q("#userName")
@@ -59,5 +170,6 @@ function displayAlert() {
 
 window.onload = () => {
     loadName();
+    displayProfilePost();
     logoutButton.onclick = logout;
 }
