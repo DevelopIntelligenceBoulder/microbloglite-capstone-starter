a154386c754cd26e89e056b982a9707b66e9da8f
Sylonjia Odion <Sodion@atl.yearup.org>
2022-12-09 13:31:05 -0500

    last

--- profile/profile.js
+++ profile/profile.js
@@ -28,7 +28,7 @@ function displayProfilePost() {
         let username = loginData.username;
 
         posts.forEach((post) => {
-            if (username == post.username) {
+            if (username != post.username) {
                 card(post, cardSection);
             }
         });
@@ -85,9 +85,9 @@ function card(section) {
             </div>
             <div class="ml-3">
               <p class="text-base leading-6 font-medium text-black  username">
-                ${section.username}
+                ${section.username} 
                 <span class="text-sm atName leading-5 font-medium text-gray-400 group-hover:text-gray-300 transition ease-in-out duration-150">
-                @${loginData.username}
+                 ${section.createdAt}
                   </span>
                    </p>
             </div>
@@ -116,7 +116,7 @@ function card(section) {
                     </div>
 
                     <div class="flex-1 text-center py-2 m-2">
-                        <a href="#" class="w-12 mt-1 group flex items-center text-gray-500 px-3 py-2 text-base leading-6 font-medium rounded-full hover:bg-blue-800 hover:text-blue-300">
+                        <a href="#" class="w-12 mt-1 group flex items-center text-gray-500 px-3 py-2 text-base leading-6 font-medium rounded-full hover:bg-blue-800 hover:text-blue-300  click:bg-red-500">
                             <svg class="text-center h-7 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
                         </a>
                     </div>
