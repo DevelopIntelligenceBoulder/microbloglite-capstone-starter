d5b68e2f4407c57966121333771fb65d0c3e38af
tarik718 <tdavis000@atl.yearup.org>
2022-12-09 09:42:40 -0500

    log out fixed

--- posts/posts.js
+++ posts/posts.js
@@ -5,6 +5,7 @@
 const searchTypeDropdown = document.getElementById("search-type-dropdown");
 const userDropdown = document.getElementById("user-dropdown");
 const userSection = document.getElementById("user-section");
+const logoutBtn = document.getElementById("logoutBtn");
 
 
 const $q = (s) => document.querySelector(s);
@@ -80,9 +81,9 @@ function fillTable(posts) {
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
-   
-   
-
+    cell1.className = "tabletext"
+    cell2.className = "tabletext"
+    cell3.className = "tabletext"
     cell3.innerText = post.createdAt;
     cell2.innerText = post.username;
     cell1.innerText = post.text;
@@ -100,4 +101,34 @@ function loadPosts() {
 window.onload = () => {
   searchTypeDropdown.onchange = selectSearchType;
   userDropdown.onchange = loadPosts;
+  logoutBtn.onclick = logout;
+
 };
+
+function logout () {
+  const loginData = getLoginData();
+
+  // GET /auth/logout
+  const options = { 
+      method: "GET",
+      headers: { 
+          // This header is how we authenticate our user with the
+          // server for any API requests which require the user
+          // to be logged-in in order to have access.
+          // In the API docs, these endpoints display a lock icon.
+          Authorization: `Bearer ${loginData.token}`,
+      },
+  };
+
+  fetch(api + "/auth/logout", options)
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .finally(() => {
+          // We're using `finally()` so that we will continue with the
+          // browser side of logging out (below) even if there is an 
+          // error with the fetch request above.
+
+          window.localStorage.removeItem("login-data");  // remove login data from LocalStorage
+          window.location.assign("/index.html");  // redirect to landing page
+      });
+}
