3fbd757823adea306834108f09e0e0a7f9334e47
nereno96 <95774571+nereno96@users.noreply.github.com>
2022-12-23 13:51:55 -0500

    login button wired up

--- landing.js
+++ landing.js
@@ -12,12 +12,12 @@ loginForm.onsubmit = function (event) {
     // We can use loginForm.username (for example) to access
     // the input element in the form which has the ID of "username".
     const loginData = {
-        username: loginForm.username.value,
-        password: loginForm.password.value,
+        username: loginForm.usernameField.value,
+        password: loginForm.passwordField.value,
     }
 
     // Disables the button after the form has been submitted already:
-    loginForm.loginButton.disabled = true;
+    loginForm.loginBtn.disabled = true;
 
     // Time to actually process the login using the function from auth.js!
     login(loginData);
