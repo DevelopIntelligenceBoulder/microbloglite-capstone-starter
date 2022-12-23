fbf8e356460f0e8c36eac8b145504d6052770e3d
nereno96 <95774571+nereno96@users.noreply.github.com>
2022-12-23 13:52:20 -0500

    button registers user

---
+++ registration.js
@@ -0,0 +1,30 @@
+/* Registration Page JavaScript */
+
+"use strict";
+
+const registrationForm = document.querySelector("#registration");
+
+registrationForm.onsubmit = function (event) {
+    // Prevent the form from refreshing the page,
+    // as it will do by default when the Submit event is triggered:
+    event.preventDefault();
+
+    const registrationData = {
+        username: registrationForm.usernameField.value,
+        password: registrationForm.passwordField.value,
+        fullName: `${registrationForm.nameField.value} ${registrationForm.lastNameField.value}`,
+    }
+
+    // Disables the button after the form has been submitted already:
+    registrationForm.signinButton.disabled = true;
+
+    register(registrationData);
+};
+
+function register(userData) {
+    fetch(`https://microbloglite.herokuapp.com/api/users`, {
+        method: "POST",
+        body: userData
+    })
+    .then(result => result.json());
+}
