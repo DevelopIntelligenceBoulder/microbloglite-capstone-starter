5cc49f36d51f3b387e83004f5e5edaf82398da74
kenny5144 <ojok761@gmail.com>
2022-12-30 08:29:51 -0500

    taking a break

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,11 @@
 /* Posts Page JavaScript */
 
 "use strict";
+window.onload = function () {
+  getusersblogs();
+};
+function getusersblogs() {
+  fetch("")
+    .then((res) => res.json)
+    .then((data) => {});
+}
