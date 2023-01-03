a6f1cfb4254a08951c9db1d7ae5e9d46ed382c4b
nereno96 <95774571+nereno96@users.noreply.github.com>
2023-01-03 13:33:29 -0500

    removed extra redirect to posts page because one already exists in auth.js

--- landing.js
+++ landing.js
@@ -22,6 +22,4 @@ loginForm.onsubmit = function (event) {
 
     // Time to actually process the login using the function from auth.js!
     login(loginData);
-
-    window.location.href = "posts/index.html";
 };
