3221a4906682fee056ee2650ba2f3ca309d4234e
AhmedS12899 <AhmedS12899@Gmail.com>
2023-01-03 11:45:16 -0500

    completed validation for registration Page

--- registration/registration.js
+++ registration/registration.js
@@ -1,19 +1,113 @@
 window.onload = () => {
-    let password = document.getElementById('passwordField1');
-    let confirmPassword = document.getElementById('passwordField2');
 
+    const form = document.getElementById('form');
+    const username = document.getElementById('username');
+    const fname = document.getElementById('fname');
+    const lname = document.getElementById('lname');
+    const password1 = document.getElementById('password');
+    const password2 = document.getElementById('password2');
 
-        //call a function that will make sure our passwords match and are valid
-        password.onchange = checkPasswords;
+    let count = 0;
+
+    form.addEventListener('submit', e => {
+        if (count != 5) {
+            count = 0;
+            e.preventDefault(); // prevents form from submitting
+            count = valdateInputs();
+        }
+        else{
+            form.submit();
+        }
+    });
+
+    //function that will set our field to error and return a message
+    const setError = (element, message) => {
+        const inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
+
+        errorDisplay.innerText = message;
+        inputControl.classList.add('error');
+        inputControl.classList.remove('success');
     }
 
-function checkPasswords(){
-    let pass = document.getElementById('passwordField1').value;
+    //function that will set our field to success
+    const setSuccess = element => {
+        const inputControl = element.parentElement;
+        const errorDisplay = inputControl.querySelector('.error');
 
-    if (pass[0] != pass[0].toUpperCase()){
-        console.log(pass[0] != pass[0].toUpperCase())
-        let passFeedback = document.getElementById('passwordField1Feedback')
-        passFeedback.className = 'invalid-feedback';
-        passFeedback.innerHTML = "Error";
+        errorDisplay.innerText = '';
+        inputControl.classList.add('success');
+        inputControl.classList.remove('error');
+    }
+    const valdateInputs = () => {
+        const userValue = username.value.trim();
+        const fnameValue = fname.value.trim();
+        const lnameValue = lname.value.trim();
+        const password1Value = password1.value.trim();
+        const password2Value = password2.value.trim();
+        //username validation
+        if (userValue === '') {
+            setError(username, 'Username is required')
+        }
+        else if (userValue.length < 6 || userValue.charAt(0) != userValue.charAt(0).toUpperCase()) {
+            setError(username, 'Username must be 6 characters or more and Uppercase')
+        }
+        else {
+            setSuccess(username);
+            count++;
+        }
+
+        //first name validation
+        if (fnameValue === '') {
+            setError(fname, 'Enter a First Name');
+        }
+        else if (fnameValue.length < 3) {
+            setError(fname, 'First Name must be 3 Characters or more');
+        }
+        else {
+            setSuccess(fname);
+            count++;
+        }
+
+        //last name validation
+        if (lnameValue === '') {
+            setError(lname, 'Enter a Last Name');
+        }
+        else if (lnameValue.length < 3) {
+            setError(lname, 'Last Name must be 3 Characters or more');
+        }
+        else {
+            setSuccess(lname);
+            count++;
+        }
+
+        //password validation
+        if (password1Value === '') {
+            setError(password1, 'Enter a Password');
+        }
+        else if (password1Value.length < 6) {
+            setError(password1, 'Password must be 6 characters or more')
+        }
+        else if (password1Value.charAt(0) != password1Value.charAt(0).toUpperCase()) {
+            setError(password1, 'Password must be uppercase')
+        }
+        else {
+            setSuccess(password1);
+            count++;
+        }
+
+        //password confirmation validation
+        if (password2Value === '') {
+            setError(password2, 'Enter a Password');
+        }
+        else if (password1Value != password2Value) {
+            setError(password2, 'Passwords do not match');
+        }
+        else {
+            setSuccess(password2);
+            count++;
+        }
+
+        return count;
     }
 }
