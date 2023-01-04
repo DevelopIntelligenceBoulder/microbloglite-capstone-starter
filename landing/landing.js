37cbc84e9ee0c6595a89a4d5d4e22a123434cdd2
kenny5144 <ojok761@gmail.com>
2023-01-04 09:48:27 -0500

    moving to another brnch to complete a feature

--- landing/landing.js
+++ landing/landing.js
@@ -5,20 +5,20 @@
 const loginForm = document.querySelector("#login");
 
 loginForm.onsubmit = function (event) {
-    // Prevent the form from refreshing the page,
-    // as it will do by default when the Submit event is triggered:
-    event.preventDefault();
+  // Prevent the form from refreshing the page,
+  // as it will do by default when the Submit event is triggered:
+  event.preventDefault();
 
-    // We can use loginForm.username (for example) to access
-    // the input element in the form which has the ID of "username".
-    const loginData = {
-        username: loginForm.username.value,
-        password: loginForm.password.value,
-    }
+  // We can use loginForm.username (for example) to access
+  // the input element in the form which has the ID of "username".
+  const loginData = {
+    username: loginForm.username.value,
+    password: loginForm.password.value,
+  };
 
-    // Disables the button after the form has been submitted already:
-    loginForm.loginButton.disabled = true;
+  // Disables the button after the form has been submitted already:
+  loginForm.loginButton.disabled = true;
 
-    // Time to actually process the login using the function from auth.js!
-    login(loginData);
+  // Time to actually process the login using the function from auth.js!
+  login(loginData);
 };
--- posts/posts.js
+++ posts/posts.js
@@ -3,11 +3,23 @@
 "use strict";
 window.onload = function () {
   getAllUsers();
+  logout();
+  let Logoutbtn = document.getElementById("Logoutbtn");
+  Logoutbtn.onclick = function () {
+    logout;
+  };
 };
 function getAllUsers() {
-  fetch("https://microbloglite.herokuapp.com/api/post", Options);
-    .then(res => res.json())
-    .then(data => {
-
-    })
+  let headingContent = document.getElementById("headingContent");
+  let contentArea = document.getElementById("contentArea");
+  let author = document.getElementById("author");
+  let timeStamp = document.getElementById("timeStamp");
+  fetch("https://microbloglite.herokuapp.com/api/post")
+    .then((res) => res.json())
+    .then((data) => {
+      headingContent.innerHTML = data._id;
+      contentArea.innerHTML = data.text;
+      author.innerHTML = data.username;
+      timeStamp.innerHTML = data.createdAt;
+    });
 }
