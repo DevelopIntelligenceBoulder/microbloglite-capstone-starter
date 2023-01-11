a6367a9094830db1c93e7b3f369f79a2e35cba63
Michael Matinez <116032312+Preslee650@users.noreply.github.com>
2023-01-11 10:25:27 -0800

    post page
    created "create post function" and dynamic bearer token

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,36 @@
 /* Posts Page JavaScript */
 
 "use strict";
+
+function CreatePost() {
+  var myHeaders = new Headers();
+
+  let loginData = getLoginData();
+  //  console.log(loginData.token);
+  //  alert(loginData);
+  myHeaders.append("accept", "application/json");
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Authorization", "Bearer " + loginData.token);
+
+  var raw = JSON.stringify({
+    text: document.getElementById("capturePost").value,
+  });
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch("https://microbloglite.herokuapp.com/api/posts", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+}
+
+// function Test() {
+//      let test = getLoginData();
+//      console.log(test);
+// }
+// Test();
