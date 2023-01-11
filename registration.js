c37f9e9bd2146fbdbdcfc3b36ce072a4fd9f00e2
Michael Matinez <116032312+Preslee650@users.noreply.github.com>
2023-01-10 16:16:10 -0800

    registration functioning
    captures user input and sends you to post.html page.

--- registration.js
+++ registration.js
@@ -1,27 +1,32 @@
+// creating new user when they register their info
 function CreateUser() {
+  let myHeaders = new Headers();
+  myHeaders.append("accept", "application/json");
+  myHeaders.append("Content-Type", "application/json");
 
-    
-let myHeaders = new Headers();
-myHeaders.append("accept", "application/json");
-myHeaders.append("Content-Type", "application/json");
+  let raw = JSON.stringify({
+    username: document.getElementById("username").value,
+    fullName: "michaelmartinez5",
+    password: "mike",
+  });
 
-let raw = JSON.stringify({
-  username: "michael5",
-  fullName: "michaelmartinez4",
-  password: "mike",
-});
+  // post request
+  let requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
 
-let requestOptions = {
-  method: "POST",
-  headers: myHeaders,
-  body: raw,
-  redirect: "follow",
-};
-alert('Creating the user');
-fetch("https://microbloglite.herokuapp.com/api/users", requestOptions)
-  .then((response) => response.text())
-  .then((data) => alert(data))
-  .catch((error) => console.log("error", error));
+  fetch("https://microbloglite.herokuapp.com/api/users", requestOptions)
+    .then((response) => response.text())
+    .then((data) => {
+      alert(data);
+      // sends you to index page
+      window.location.href = "/index.html";
+    })
+    .catch((error) => console.log("error", error));
+  // storing users info
+  window.localStorage.setItem(username, raw);
+  window.localStorage.getItem(username);
 }
-
-   // window.localStorage.setItem();
