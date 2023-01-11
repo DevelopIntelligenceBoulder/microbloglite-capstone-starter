d7f4a7ea3084b6ef5ad71263bf1dcd418b17dac8
Michael Matinez <116032312+Preslee650@users.noreply.github.com>
2023-01-11 11:35:48 -0800

    waiting for remsey
    api may not functioning to allow new post to be sent to database.
    
    post page displays post when you click on create post button.

--- posts/posts.js
+++ posts/posts.js
@@ -2,21 +2,20 @@
 
 "use strict";
 
+// function authorizes that user is logged in and captures when they create a post
 function CreatePost() {
-  var myHeaders = new Headers();
-
+  let myHeaders = new Headers();
+  // token authorization
   let loginData = getLoginData();
-  //  console.log(loginData.token);
-  //  alert(loginData);
   myHeaders.append("accept", "application/json");
   myHeaders.append("Content-Type", "application/json");
   myHeaders.append("Authorization", "Bearer " + loginData.token);
 
-  var raw = JSON.stringify({
+  let raw = JSON.stringify({
     text: document.getElementById("capturePost").value,
   });
 
-  var requestOptions = {
+  let requestOptions = {
     method: "POST",
     headers: myHeaders,
     body: raw,
@@ -27,10 +26,31 @@ function CreatePost() {
     .then((response) => response.text())
     .then((result) => console.log(result))
     .catch((error) => console.log("error", error));
+  DisplayAllPost();
 }
 
-// function Test() {
-//      let test = getLoginData();
-//      console.log(test);
-// }
-// Test();
+function DisplayAllPost() {
+  let element = document.getElementById("displayPostHere");
+
+  let myHeaders = new Headers();
+
+  let loginData = getLoginData();
+  myHeaders.append("accept", "application/json");
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Authorization", "Bearer " + loginData.token);
+
+  let requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch("https://microbloglite.herokuapp.com/api/posts", requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      for (let i = 0; i < result.length; i++) {
+        let userPostInfo = `${result[i].username} <br> ${result[i].createdAt} <br> ${result[i].text} <br>`;
+        element.innerHTML += userPostInfo + "<br>";
+      }
+    });
+}
