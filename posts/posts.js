8a79a1f7526f32d656881fddab341411e9b0eef8
christiantsg <kanemarinp2002@gmail.com>
2023-01-19 14:11:21 -0800

    comments added for what a code is doing
    comments applied in post.js and index.html

--- posts/posts.js
+++ posts/posts.js
@@ -2,6 +2,73 @@
 
 "use strict";
 
+//This function is used to make a GET request to a server
+
+function DisplayAllPost() {
+  let element = document.getElementById("displayPostHere");
+
+  let myHeaders = new Headers();
+
+  let loginData = getLoginData();
+  myHeaders.append("accept", "application/json");
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Authorization", "Bearer " + loginData.token);
+
+  let requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  //This code is making a fetch request to the specified url with the given requestOptions.
+  
+  fetch(
+    "https://microbloglite.herokuapp.com/api/posts?limit=2000&offset=0",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => document.getElementById("post").innerHTML = result.map(postTemplate).join(" "))
+
+//post template section for the card including name, text and when its created
+
+}
+DisplayAllPost();
+
+function postTemplate(post) {
+  return `<div class="card">
+<div> 
+<h3 class="card-title">${post.username}</h3>
+</div>
+<p class="card-description">${post.text}</p>
+<p>${post.createdAt}</p>
+</div>
+</div>`
+  element.innerHTML += postTemplate;
+}
+
+
+
+
+
+//   for (let i = 0; i < result.length; i++) {
+
+  //     let userPostInfo = `${result[i].username} <br> ${result[i].createdAt} <br> ${result[i].text} <br>`;
+  //     element.innerHTML += userPostInfo + "<br>";
+
+  //   }
+  // });
+
+// let element = document.getElementById("displayPostHere");
+        // let userPostInfo =  `
+        //     <div id="user-post-info">
+        //         <div>
+        //             <span id="username">${result[i].username}</span>
+        //             <span id="created-at">${result[i].createdAt}</span>
+        //         </div>
+        //         <div id="text">${result[i].text}</div>
+        //     </div>`;
+        // element.innerHTML += userPostInfo;
+
 // // function authorizes that user is logged in and captures when they create a post
 // function CreatePost() {
 //   let myHeaders = new Headers();
@@ -28,67 +95,3 @@
 //     .catch((error) => console.log("error", error));
 //   DisplayAllPost();
 // }
-
-function DisplayAllPost() {
-  let element = document.getElementById("displayPostHere");
-
-  let myHeaders = new Headers();
-
-  let loginData = getLoginData();
-  myHeaders.append("accept", "application/json");
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("Authorization", "Bearer " + loginData.token);
-
-  let requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow",
-  };
-
-  fetch(
-    "https://microbloglite.herokuapp.com/api/posts?limit=2000&offset=0",
-    requestOptions
-  )
-    .then((response) => response.json())
-    .then((result) => document.getElementById("post").innerHTML = result.map(postTemplate).join(" "))
-
-
-  //   for (let i = 0; i < result.length; i++) {
-
-  //     let userPostInfo = `${result[i].username} <br> ${result[i].createdAt} <br> ${result[i].text} <br>`;
-  //     element.innerHTML += userPostInfo + "<br>";
-
-  //   }
-  // });
-}
-DisplayAllPost();
-
-function postTemplate(post) {
-  return `<div class="card">
-<div> 
-<h3 class="card-title">${post.username}</h3>
-</div>
-<p class="card-description">${post.text}</p>
-<p>${post.createdAt}</p>
-</div>
-</div>`
-  element.innerHTML += postTemplate;
-}
-
-
-
-
-
-
-
-// let element = document.getElementById("displayPostHere");
-        // let userPostInfo =  `
-        //     <div id="user-post-info">
-        //         <div>
-        //             <span id="username">${result[i].username}</span>
-        //             <span id="created-at">${result[i].createdAt}</span>
-        //         </div>
-        //         <div id="text">${result[i].text}</div>
-        //     </div>`;
-        // element.innerHTML += userPostInfo;
-
