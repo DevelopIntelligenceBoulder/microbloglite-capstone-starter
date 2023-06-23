3d2da099fb8fa801f1639b809ecb98357fd00e7f
cindy <Cdiaz@appdev.yearup.org>
2023-06-23 16:24:19 -0400

    added fetch post method to create post

--- Profile/profile.js
+++ Profile/profile.js
@@ -1,1 +1,62 @@
 "use strict";
+
+const bio = document.getElementById("bio");
+const postDescription = document.getElementById("postDescription");
+const postBtn = document.getElementById("postBtn");
+const postPage = document.getElementById("postPage");
+const logOut = document.getElementById("logOut");
+
+const loginData = getLoginData();
+const token = loginData.token;
+
+
+window.onload = function () {
+    console.log("window loaded")
+
+    postBtn.onclick = onPostBtnClick;
+    logOut.onclick = onLogoutClick
+
+}
+
+function onPostBtnClick() {
+    //create a new todo using the API!
+
+    // Create JSON object to include in the request body
+
+
+    let bodyData = {
+
+
+        "text": postDescription.value
+
+
+    }
+
+
+    // Send the request
+    fetch(apiBaseURL + "/api/posts", {
+        method: "POST",
+        body: JSON.stringify(bodyData),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            Authorization: `Bearer ${loginData.token}`
+        },
+    })
+        .then(response => response.json())
+        .then(post => {
+            // If the POST finishes successfully, display a message
+
+            console.log(post)
+
+
+        });
+
+}
+
+
+
+function onLogoutClick() {
+  logout();
+}
+
+
