56b48f811481376f5782eb30258d8463ffa21052
davempark09 <43024345+davempark09@users.noreply.github.com>
2023-06-23 15:05:46 -0400

    Logout Button

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,35 @@
 /* Posts Page JavaScript */
 
 "use strict";
+
+const logoutButton = document.querySelector("#logout")
+
+function logout () {
+    const loginData = getLoginData();
+
+    // GET /auth/logout
+    const options = { 
+        method: "GET",
+        headers: { 
+            // This header is how we authenticate our user with the
+            // server for any API requests which require the user
+            // to be logged-in in order to have access.
+            // In the API docs, these endpoints display a lock icon.
+            Authorization: `Bearer ${loginData.token}`,
+        },
+    };
+
+    fetch(apiBaseURL + "/auth/logout", options)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .finally(() => {
+            // We're using `finally()` so that we will continue with the
+            // browser side of logging out (below) even if there is an 
+            // error with the fetch request above.
+
+            window.localStorage.removeItem("login-data");  // remove login data from LocalStorage
+            window.location.assign("/");  // redirect back to landing page
+        });
+}
+
+logoutButton.onclick = logout
