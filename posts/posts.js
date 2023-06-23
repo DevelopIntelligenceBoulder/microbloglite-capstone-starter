cb1ab5faf8176cda76340965bcdaf7051e8fadc3
Deshonda Holmes <Deshonda.holmes@gmail.com>
2023-06-23 13:12:46 -0400

    Added link and button

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,27 @@
 /* Posts Page JavaScript */
 
 "use strict";
+
+
+
+window.onload =  () =>{
+    console.log("hello");
+}
+
+function getAllUsers() {
+    // GET /api/users
+    const loginData = getLoginData();
+    const options = {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${loginData.token}`,
+        },
+    };
+    // note: the api variable is defined in auth.js
+    fetch(api + "/api/users", options)
+        .then(response => response.json())
+        .then(users => {
+            // Do something with the users array...
+            console.log(users);
+        });
+}
