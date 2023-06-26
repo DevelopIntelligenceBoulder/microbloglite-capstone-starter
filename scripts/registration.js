4fe793f2f73217bdaf6d5d101b025f6496189565
Nerio Guio <andrik1379@gmail.com>
2023-06-26 16:03:25 -0400

    register java initial

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,1 +1,28 @@
 "use strict";
+const usernameInputEl = document.getElementById("floatingInput");
+const passwordInputEl = document.getElementById("floatingPassword");
+const fullnameInputEl = document.getElementById("fullnameInput");
+const registerBtnEl = document.getElementById("registerButton");
+
+// registerBtnEl.onclick = register;
+
+registerBtnEl.addEventListener("click", () => {
+    let bodyData = {
+        username: usernameInputEl.value,
+        fullName: fullnameInputEl.value,
+        password: passwordInputEl.value,
+    };
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(bodyData),
+    };
+
+    return fetch("https://microbloglite.herokuapp.com/api/users", options)
+        .then((response) => response.json())
+        .then((loginData) => {
+            window.location.assign("../index.html");
+        });
+});
