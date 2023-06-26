06429de73c59dd32b72b5aa916d0d47885eeeb40
imalicx <xenrecords@yahoo.com>
2023-06-26 10:23:01 -0400

    pulling

--- scripts/posts.js
+++ scripts/posts.js
@@ -1,2 +1,13 @@
 "use strict";
 
+// logout button
+const logoutButton = document.getElementById("logoutButton");
+logoutButton.addEventListener("click", () => {
+  logout();
+  
+});
+
+// display posts to all users with a fetch
+fetch("https://microbloglite.herokuapp.com/api/users")
+  .then(response => response.json())
+  .then(users => {
