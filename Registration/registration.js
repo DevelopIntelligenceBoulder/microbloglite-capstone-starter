c66f1e068857e00570d0d5f3575300189eb45c7a
acruz-23 <cruz.581@outlook.com>
2023-06-26 11:06:37 -0400

    JS makes fetch request to create a new user, user is returned to the login page after account creation, a basic error handler was added for failed fetch requests.

--- Registration/registration.js
+++ Registration/registration.js
@@ -1,6 +1,8 @@
 console.log("js working");
 
 const registerForm = document.querySelector("#register");
+const messagePEl = document.getElementById("messageP");
+const apiBaseURL = "https://microbloglite.herokuapp.com";
 
 registerForm.onsubmit = function (event) {
   event.preventDefault();
@@ -9,10 +11,27 @@ registerForm.onsubmit = function (event) {
     registerForm.registrationPassword.value
   )
     return;
-  console.log("passwords are the same");
-  const body = {
+  const registrationBody = {
     username: registerForm.registrationUsername.value,
     fullName: registerForm.registrationFullname.value,
     password: registerForm.registrationPassword.value,
   };
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(registrationBody),
+  };
+  fetch(apiBaseURL + "/api/users", options)
+    .then((response) => response.json())
+    .then((acctCreation) => {
+      messagePEl.textContent = "Success returning to login";
+      setTimeout(() => {
+        window.location.href = "../index.html";
+      }, 1000);
+    })
+    .catch((err) => {
+      messagePEl.textContent = "An error has occured Please try again later";
+    });
 };
