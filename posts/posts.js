9a65c00f0a3619be1922a02f1ade84430bf1db4b
Paris Cabrera <marvel729@gmail.com>
2023-06-26 13:16:03 -0400

    posts page changes

--- posts/posts.js
+++ posts/posts.js
@@ -4,30 +4,36 @@ window.onload = function () {
   getAllPost();
 };
 
-function getAllPost() {
-  return fetch(
-    "https://microbloglite.herokuapp.com/api/posts?limit=100&offset=0"
-  )
+function getAllPost(accessPost) {
+  const options = {
+    method: "POST",
+    headers: {
+      // This header specifies the type of content we're sending.
+      // This is required for endpoints expecting us to send
+      // JSON data.
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(accessPost),
+
+  };
+  
+  return fetch(apiBaseURL + "api/posts" + options)
     .then((response) => response.json())
     .then((data) => {
-      loadAllPost(data)
-      console.log(data)
+      loadAllPost(data);
+      console.log(data);
     })
     .catch((error) => {
       console.error("Error fetching users:", error);
     });
-
 }
 
-const parentElement = document.querySelector("main")
+const parentElement = document.querySelector("main");
 
 function loadAllPost(posts) {
-
-  for(post of posts) {
-    
-    const mainCard = document.createElement("div")
-      mainCard.classList.add("mainCard")
-      parentElement.append(mainCard)
-
+  for (post of posts) {
+    const mainCard = document.createElement("div");
+    mainCard.classList.add("mainCard");
+    parentElement.append(mainCard);
   }
 }
