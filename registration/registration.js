819cb63a2e927c0ec07b8c430c7767145713aa17
Bemnet Erbeto <bemneterbeto@gmail.com>
2023-06-26 01:07:13 -0400

    bug fix

--- registration/registration.js
+++ registration/registration.js
@@ -42,10 +42,10 @@ const registerUser = async () => {
       if (response.ok) {
         newAccAlert.classList.remove('hide')
             setTimeout(() => {
-                window.open('../index.html', '_blank')
+                window.location.href = '../index.html'
             }, 2000);
       } else {
-        if (!((newAccAlert.contains('hide')))){
+        if (!(newAccAlert.classList.contains('hide'))){
             newAccAlert.classList.add('hide')
         }
         const errorData = await response.json();
