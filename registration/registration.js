4f06281eb3f27287bfc3ebe559658ddb11e8cab1
Bemnet Erbeto <bemneterbeto@gmail.com>
2023-06-25 23:01:54 -0400

    added password show hide feature

--- registration/registration.js
+++ registration/registration.js
@@ -1,3 +1,21 @@
 function createAccount() {
   
 }
+
+let passShow = document.getElementById('passShow')
+let passHide = document.getElementById('passHide')
+let passInput = document.getElementById('exampleInputPassword1')
+
+function passShowClick(){
+    passShow.classList.add('hide')
+    passHide.classList.remove('hide')
+    passInput.type = 'text'
+
+}
+
+function passHideClick(){
+    passInput.type = 'password'
+    passHide.classList.add('hide')
+    passShow.classList.remove('hide')
+    
+}
