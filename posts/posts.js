0ace39f6472c561d9d51fd451a84bd2382653c33
Aazaad06 <aazaadgass@gmail.com>
2023-06-26 15:22:55 -0400

    did some Js

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,31 @@
 /* Posts Page JavaScript */
 
 "use strict";
+
+
+const postContainer = document.querySelector('#postContainer')
+
+function postFetch () {
+    const loginData = getLoginData();
+  console.log(JSON.parse(loginData.token))
+    
+    //const options = { 
+        //method: "GET",
+        //headers: { 
+            
+            //Authorization: `Bearer ${loginData.token}`,
+       // },
+   // };
+
+    //fetch(apiBaseURL + "/auth/logout", options)
+        //.then(response => response.json())
+       // .then(data => console.log(data))
+        //.finally(() => {
+           
+
+           // window.localStorage.removeItem("login-data");  
+           // window.location.assign("/");  
+       /// });
+}
+
+postFetch()
