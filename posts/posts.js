632cc34051e31cda7a204384de1dcdbc2e8d8739
yoitshavi <aboyer@appdev.yearup.org>
2023-06-26 14:18:44 -0400

    added posts to page

--- posts/posts.js
+++ posts/posts.js
@@ -7,12 +7,18 @@
 const textboxEl = document.getElementById("textbox");
 const postBtnEl = document.getElementById("postBtn");
 const postsMadeUlEl = document.getElementById("postsMadeUl");
+const logoutBtnEl = document.getElementById("logoutBtn");
+const postCardsDivEl = document.getElementById("postCards");
 
-postBtnEl.onclick = getPosts;
+const likeBtn = document.createElement("BUTTON");
+
+logoutBtnEl.onclick = logout;
+const loginData = getLoginData();
+// postBtnEl.onclick =;
+window.onload = getPosts;
 
 function getPosts() {
-  const loginData = getLoginData();
-
+  console.log(loginData);
   const options = {
     method: "GET",
     headers: {
@@ -31,6 +37,77 @@ function getPosts() {
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      //   console.log(data);
+      data.forEach((post) => {
+        console.log(post);
+        const card = document.createElement("div");
+        card.classList = "row";
+        // card.value = post._id;
+
+        const content = ` <div class="col">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title" value="${post._id}">${post.username}</h5>
+            <p class="card-text">
+              ${post.text}
+            </p>
+            <input type="button" value="Like" id="likeBtn"/>
+          </div>
+        </div>
+      </div>`;
+
+        // const like = document.createElement("BUTTON");
+        // var t = document.createTextNode("Like");
+        // //   btnUpdate.id = 'update0';
+        // like.appendChild(t);
+        // likeButton.appendChild(like);
+
+        postCardsDivEl.innerHTML += content;
+      });
     });
 }
+
+// postBtnEl.addEventListener("click", () => {
+//   let bodyData = {
+//     text: textboxEl.value,
+//     username: loginData.username,
+//   };
+//   const options = {
+//     method: "POST",
+//     body: JSON.stringify(bodyData),
+//     headers: {
+//       "Content-type": "application/json; charset=UTF-8",
+//       Authorization: `Bearer ${loginData.token}`,
+//     },
+//   };
+//   const url = "https://microbloglite.herokuapp.com/api/posts";
+//   fetch(url, options)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       window.location.assign("/posts");
+//     });
+// });
+
+// const likeBtnEl = document.getElementById("likeBtn");
+// const cardId = document.getElementsByClassName("card-title");
+
+// likeBtnEl.addEventListener("click", () => {
+//   let bodyData = {
+//     postId: cardId.value,
+//   };
+//   const options = {
+//     method: "POST",
+//     body: JSON.stringify(bodyData),
+//     headers: {
+//       "Content-type": "application/json; charset=UTF-8",
+//       Authorization: `Bearer ${loginData.token}`,
+//     },
+//   };
+//   const url = "https://microbloglite.herokuapp.com/api/likes";
+//   fetch(url, options)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//       window.location.assign("/posts");
+//     });
+// });
