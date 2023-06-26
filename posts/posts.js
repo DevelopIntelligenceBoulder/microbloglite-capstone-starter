5efab39b6043905e21435c10e61157eb404821a3
Dillon Coe <SupersonicC123@gmail.com>
2023-06-26 14:44:59 -0400

    added functional logout and new post btns

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,28 @@
 /* Posts Page JavaScript */
 
 "use strict";
+
+document.getElementById("logoutBtn").addEventListener("click", () => {
+  logout();
+});
+
+document.getElementById("newPostBtn").addEventListener("click", () => {
+  location.href = "./new-post.html";
+});
+
+const postsUrl =
+  "https://microbloglite.herokuapp.com/api/posts?limit=100&offset=0";
+
+if (isLoggedIn()) {
+  const authData = getLoginData();
+  const options = {
+    method: "GET",
+    headers: { Authorization: `Bearer ${authData.token}` },
+  };
+  fetch(postsUrl, options)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach((post) => {});
+    });
+}
