22ded3ec4198da27b6ca7a6eaba99ea1079529ae
imalicx <xenrecords@yahoo.com>
2023-06-26 14:57:34 -0400

    updatedposts.js

--- scripts/posts.js
+++ scripts/posts.js
@@ -1,2 +1,29 @@
 "use strict";
 
+// prevents access to page unless visitor is logged in
+if (isLoggedIn() === true) {
+    window.location.replace("/registration");
+}
+
+// logout button event listener
+const logoutButton = document.getElementById("logoutButton");
+logoutButton.addEventListener("click", () => {
+  logout();
+});
+
+// Fetch and display posts
+const postsContainer = document.getElementById("posts");
+
+fetch("https://microbloglite.herokuapp.com/api/posts")
+  .then(response => response.json())
+  .then(posts => {
+    posts.forEach(post => {
+      const postElement = document.createElement("div"); 
+      postElement.textContent = post.text;
+      postsContainer.appendChild(postElement);
+    });
+  })
+  .catch(error => {
+    console.error("Error fetching posts:", error);
+  });
+
