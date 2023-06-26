0bbdf41b6623d6fe7f16c9425aeaf4ed3b7f163c
Bemnet Erbeto <bemneterbeto@gmail.com>
2023-06-25 23:47:19 -0400

    added authentication system to add a new user

--- registration/registration.js
+++ registration/registration.js
@@ -19,3 +19,35 @@ function passHideClick(){
     passShow.classList.remove('hide')
     
 }
+
+// registration 
+
+const registerUser = async () => {
+    
+    const fullName = document.getElementById('exampleInputfullname').value
+    const username = document.getElementById('exampleInputUsername').value
+    const password = document.getElementById('exampleInputPassword1').value
+
+    try {
+      const response = await fetch('https://microbloglite.herokuapp.com/api/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ fullName, username, password })
+      });
+  
+      if (response.ok) {
+        window.open('../index.html', '_blank')
+        let files = response.json()
+        console.log(files)
+      } else {
+        const errorData = await response.json();
+        console.error('Registration failed:', errorData.message);
+ 
+      }
+    } catch (error) {
+      console.error('Registration failed:', error);
+
+    }
+  };
