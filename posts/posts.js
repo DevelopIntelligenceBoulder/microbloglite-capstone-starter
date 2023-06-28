458f581a57ae77edc1f59b29a6ba9ee438a3a89c
gioalers <101071287+gioalers@users.noreply.github.com>
2023-06-28 13:35:16 -0400

    last

--- posts/posts.js
+++ posts/posts.js
@@ -17,54 +17,6 @@ function countLikes(likes) {
     return likes.length;
 }
 
-/*function updateLikesCount(likesCountElement, likes) {
-    const count = countLikes(likes);
-    likesCountElement.textContent = count;
-  }
-  
-  function handleLikeButtonClick(postId, likesCountElement, likeButton) {
-    const loginData = getLoginData();
-    const options = {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${loginData.token}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ postId }),
-    };
-  
-    fetch(apiBaseURL + `/api/posts/${postId}/like`, options)
-      .then(response => response.json())
-      .then(data => {
-        if (data.success) {
-          // Update the UI with the new likes count
-          updateLikesCount(likesCountElement, data.likes);
-          // Toggle the like button's appearance
-          likeButton.classList.toggle("liked");
-        } else {
-          // Handle any error response from the API
-          console.error(data.error);
-        }
-      })
-      .catch(error => {
-        // Handle fetch or network errors
-        console.error(error);
-      });
-  }
-  
-function attachLikeButtonEvents() {
-    const likeButtons = document.querySelectorAll(".like-button");
-
-    likeButtons.forEach(likeButton => {
-        likeButton.addEventListener("click", () => {
-            const postElement = likeButton.closest(".card");
-            const likesCountElement = postElement.querySelector(".likes-count");
-            const postId = postElement.dataset.postId;
-
-            handleLikeButtonClick(postId, likesCountElement, likeButton);
-        });
-    });
-}*/
 
 function postFetch() {
     const loginData = getLoginData();
