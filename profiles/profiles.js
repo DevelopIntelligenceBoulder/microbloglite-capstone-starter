bb903bccd14bd14ec06db678f43e8760054d7499
Dillon Coe <SupersonicC123@gmail.com>
2023-06-28 15:35:14 -0400

    combined new-posts and profile scripts

--- profiles/profiles.js
+++ profiles/profiles.js
@@ -1,5 +1,35 @@
-const logoutBtn = document.getElementById("logoutBtn")
+const form = document.querySelector("form");
+const postArea = document.getElementById("newContent");
 
-logoutBtn.addEventListener('click', () => {
-  logout()
-})
+const logoutBtn = document.getElementById("logoutBtn");
+
+logoutBtn.addEventListener("click", () => {
+  logout();
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const bodyData = {
+    text: postArea.value,
+  };
+
+  async function makeNewPost() {
+    const authData = getLoginData();
+    const options = {
+      method: "POST",
+      body: JSON.stringify(bodyData),
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${authData.token}`,
+      },
+    };
+    const res = await fetch(url, options);
+    const data = await res.json();
+    return data;
+  }
+
+  if (isLoggedIn()) {
+    makeNewPost();
+  }
+});
