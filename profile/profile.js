b7a6a1e360904bc5bb5f28faaf90001c93b42bc2
Andrew Muok <Mandrew842@gmail.com>
2023-06-28 15:57:01 -0400

    made function to let user make post

--- profile/profile.js
+++ profile/profile.js
@@ -6,3 +6,78 @@ function getuser() {
       ///does stuff
     });
 }
+function createPost() {
+  const form = document.querySelector(".admin-form");
+  const titleInput = document.getElementById("title");
+  const bodyInput = document.getElementById("post-editor");
+  const topicSelect = document.getElementById("topic");
+  const publishedCheckbox = document.getElementById("published");
+
+  form.addEventListener("submit", event => {
+    event.preventDefault();
+
+    const errors = [];
+    if (titleInput.value.trim() === "") {
+      errors.push("Title is required");
+    }
+    if (topicSelect.value.trim() === "") {
+      errors.push("Topic is required");
+    }
+
+    const formErrors = document.querySelector(".form-errors");
+    formErrors.innerHTML = "";
+    if (errors.length > 0) {
+      errors.forEach(error => {
+        const li = document.createElement("li");
+        li.textContent = error;
+        formErrors.appendChild(li);
+      });
+      return;
+    }
+
+    const post = {
+      title: titleInput.value.trim(),
+      body: bodyInput.value.trim(),
+      topic: topicSelect.value.trim(),
+      published: publishedCheckbox.checked,
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(post),
+    };
+
+    fetch(apiBaseURL + "/api/posts", options)
+      .then(response => response.json())
+      .then(data => {
+        console.log("Post created successfully:", data);
+
+        titleInput.value = "";
+        bodyInput.value = "";
+        topicSelect.value = "";
+        publishedCheckbox.checked = false;
+
+        fetch(apiBaseURL + "/api/posts")
+          .then(response => response.json())
+          .then(posts => {
+            console.log("Fetched posts:", posts);
+            displayPosts(posts);
+          })
+          .catch(error => {
+            console.log("Error fetching posts:", error);
+          });
+      })
+      .catch(error => {
+        console.log("Error creating post:", error);
+      });
+  });
+}
+
+function displayPosts(posts) {
+  console.log("Displaying posts:", posts);
+}
+
+createPost();
