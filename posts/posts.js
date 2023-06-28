78eb0fe892b4e6972ae9532a3d306693a3b7bf49
Bemnet Erbeto <bemneterbeto@gmail.com>
2023-06-28 16:07:00 -0400

    functionalities added: scroll to top, like dislike button, posting likes/dislikes to db, minor fixes

--- posts/posts.js
+++ posts/posts.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
   const accessToken = JSON.parse(window.localStorage.getItem("login-data")).token;
   getAllPosts(accessToken);
 
@@ -25,6 +26,7 @@ let postTextarea = document.getElementById('postTextarea').value
   .then(data =>{
     let parEl = document.getElementById('postParent')
     createPost(data, parentElement)
+    console.log(data)
   }).catch (error => {
     console.log(error)
   })
@@ -35,7 +37,7 @@ function createPost(post) {
   const postContainer = document.getElementById("postContainer");
 
   const cardContainer = document.createElement("div");
-  cardContainer.classList.add("card", "mb-3", "userInfo");
+  cardContainer.classList.add("card", "userInfo");
 
   const cardBody = document.createElement("div");
   cardBody.classList.add("card-body");
@@ -58,15 +60,105 @@ function createPost(post) {
   let subName = uName.slice(1)
   userName.textContent = fLetter + subName;
 
-  const likesArea = document.createElement("h6");
-  likesArea.classList.add("card-subtitle");
-  //  likesArea.textContent = `Likes: ${post.likes.}`;
+//  like funcitonality
+
+const likesArea = document.createElement("div");
+likesArea.classList.add("card-subtitle");
+
+const likeBtn = document.createElement('img')
+likeBtn.setAttribute('src', '../assets/like.svg')
+likeBtn.setAttribute('class', 'likeBtn')
+likesArea.appendChild(likeBtn)
+
+const spanElLike = document.createElement('span')
+spanElLike.setAttribute('id', 'spanElLike')
+spanElLike.textContent = 0
+
+likeBtn.addEventListener('click', function () {
+  if (spanElDislike.textContent == 0){
+    spanElLike.textContent = 1
+  } else if (spanElDislike.textContent == 1) {
+    spanElDislike.textContent = 0
+    spanElLike.textContent = 1
+  }
+  sendReaction(post._id, 'like')
+  })
+likesArea.appendChild(spanElLike)
+
+
+const dislikeBtn = document.createElement('img')
+dislikeBtn.setAttribute('src', '../assets/dislike.svg')
+dislikeBtn.setAttribute('class', 'dislikeBtn')
+likesArea.appendChild(dislikeBtn)
+const spanElDislike = document.createElement('span')
+spanElDislike.setAttribute('id', 'spanElDislike')
+spanElDislike.textContent = 0
+dislikeBtn.addEventListener('click', function() {
+  if (spanElLike.textContent == 0){
+    spanElDislike.textContent = 1
+  } else if (spanElLike.textContent == 1) {
+    spanElLike.textContent = 0
+    spanElDislike.textContent = 1
+  }
+  sendReaction(post._id, 'dislike')
+})
+likesArea.appendChild(spanElDislike)
+
+
+
+// end
 
   cardBody.append(userName, postText, likesArea, postDate);
 
   cardContainer.appendChild(cardBody);
   postContainer.insertBefore(cardContainer, postContainer.firstChild);
 }
+
+
+
+function sendReaction(postId, reaction) {
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ postId, reaction })
+  };
+
+  fetch(apiBaseURL + "/api/likes", options)
+    .then(response => {
+      if (response.ok) {
+        console.log("Reaction saved successfully");
+        console.log(response)
+      } else {
+        throw new Error("Failed post reaction");
+      }
+    })
+    .catch(error => {
+      console.error("Error occured:", error);
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Get all post
@@ -138,4 +230,9 @@ window.addEventListener('scroll', function() {
     document.getElementById('backToTop').classList.add('hide')
 
   }
+})
+
+let backToTop = document.getElementById('backToTop')
+backToTop.addEventListener("click", function(){
+  window.scrollTo({top: 0, behavior: "smooth"})
 })
