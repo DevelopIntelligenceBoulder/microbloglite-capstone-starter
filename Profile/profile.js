6c596d2e2237f00acb5e0168d181783c5f40f805
cindy <Cdiaz@appdev.yearup.org>
2023-06-29 09:23:08 -0400

    fixed a bio update bug

--- Profile/profile.js
+++ Profile/profile.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Getting references to various HTML elements using their IDs
 const bio = document.getElementById("bio");
 const postDescription = document.getElementById("postDescription");
 const postBtn = document.getElementById("postBtn");
@@ -11,11 +12,11 @@ const editBioInput = document.getElementById("editBioInput");
 const editBioBtn = document.getElementById("editBioBtn");
 const updateBioBtn = document.getElementById("updateBioBtn");
 
-
+// Retrieving login data
 const loginData = getLoginData();
 const token = loginData.token;
 
-
+//Code to be executed when the window finishes loading
 window.onload = function () {
     console.log("window loaded")
 
@@ -24,9 +25,11 @@ window.onload = function () {
     editBioBtn.onclick = onEditButtonClick;
     updateBioBtn.onclick = onUpdateBioBtnClick;
 
-    editBioContainer.style.display = "none";//hides the  bio editcontainer
+    //hides the  bio editcontainer
+    editBioContainer.style.display = "none";
 
-    const storedBio = localStorage.getItem("bio");
+    // Retrieve and display stored bio from local storage
+    const storedBio = localStorage.getItem(`bio_${loginData.username}`);
     if (storedBio) {
         bioText.textContent = storedBio;
     }
@@ -35,8 +38,7 @@ window.onload = function () {
 }
 
 function onPostBtnClick() {
-    //create a new todo using the API!
-
+    
     // Create JSON object to include in the request body
 
 
@@ -49,7 +51,7 @@ function onPostBtnClick() {
     }
 
 
-    // Send the request
+    // Send a POST request to the API to create a new post
     fetch(apiBaseURL + "/api/posts", {
         method: "POST",
         body: JSON.stringify(bodyData),
@@ -60,8 +62,8 @@ function onPostBtnClick() {
     })
         .then(response => response.json())
         .then(post => {
-            // If the POST finishes successfully, display a message
-
+            
+            // If the POST finishes successfully, redirect to post page
             console.log(post)
             window.location.replace("../posts/index.html");
 
@@ -74,7 +76,7 @@ function onPostBtnClick() {
 
 
 function onEditButtonClick(){
-
+    // Set the value of the edit bio input box to the current bio text
     editBioInput.value = bioText.textContent;
 
     // Hide the bio text and show the edit bio input box
@@ -115,7 +117,9 @@ function onUpdateBioBtnClick(){
          // Update the bio text with the updated value
          bio.textContent = editBioInput.value;
  
-         localStorage.setItem("bio", editBioInput.value);
+          // Store the updated bio in local storage
+         localStorage.setItem(`bio_${username}`, editBioInput.value);
+
          // Hide the edit bio input box and show the updated bio text content
          bioText.style.display = "block";
          editBioContainer.style.display = "none";
