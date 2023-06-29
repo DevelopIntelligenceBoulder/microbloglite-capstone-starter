da1eb927e71531647974bd6b5378b28a4d82cb49
davempark09 <43024345+davempark09@users.noreply.github.com>
2023-06-29 11:06:12 -0400

    Likes Button Stuff and Textbox Sizing

--- posts/posts.js
+++ posts/posts.js
@@ -17,58 +17,8 @@ function countLikes(likes) {
     return likes.length;
 }
 
-/*function updateLikesCount(likesCountElement, likes) {
-    const count = countLikes(likes);
-    likesCountElement.textContent = count;
-  }
-  
-  function handleLikeButtonClick(postId, likesCountElement, likeButton) {
-    const loginData = getLoginData();
-    const options = {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${loginData.token}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ postId }),
-    };
-  
-    fetch(apiBaseURL + `/api/posts/${postId}/like`, options)
-      .then(response => response.json())
-      .then(data => {
-        if (data.success) {
-          // Update the UI with the new likes count
-          updateLikesCount(likesCountElement, data.likes);
-          // Toggle the like button's appearance
-          likeButton.classList.toggle("liked");
-        } else {
-          // Handle any error response from the API
-          console.error(data.error);
-        }
-      })
-      .catch(error => {
-        // Handle fetch or network errors
-        console.error(error);
-      });
-  }
-  
-function attachLikeButtonEvents() {
-    const likeButtons = document.querySelectorAll(".like-button");
-
-    likeButtons.forEach(likeButton => {
-        likeButton.addEventListener("click", () => {
-            const postElement = likeButton.closest(".card");
-            const likesCountElement = postElement.querySelector(".likes-count");
-            const postId = postElement.dataset.postId;
-
-            handleLikeButtonClick(postId, likesCountElement, likeButton);
-        });
-    });
-}*/
-
 function postFetch() {
     const loginData = getLoginData();
-    console.log(loginData.token);
 
     const options = {
         method: "GET",
@@ -81,28 +31,108 @@ function postFetch() {
         .then(response => response.json())
         .then(posts => {
             posts.forEach(post => {
-                const cardHTML = `
-            <div class="card text-center" id="cards" data-post-id="${post._id}">
-                <div class="card-header">
+                console.log(window.localStorage.getItem((post._id)))
+                if(window.localStorage.getItem(post._id) === null) {
+                    const cardHTML = `
+                    <div class="card text-center" id="cards" data-post-id="${post._id}">
+                    <div class="card-header">
                     <b>@${post.username}</b>
-                </div>
-                <div class="card-body">
+                    </div>
+                    <div class="card-body">
                     <p class="card-text">${post.text}</p>
-                </div><br>
-                <div class="card-footer text-muted">
+                    </div><br>
+                    <div class="card-footer text-muted">
                     ${convertDateTime(post.createdAt)}<br>
                     <span class="likes-count">${countLikes(post.likes)} Likes</span>
-                    <button class="like-button">❤</button>
-                </div>
-            </div>`;
-                postContainer.innerHTML += cardHTML;
+                    <button onmouseover="mouseOverEffect('${post._id}')" onmouseout="mouseOutEffect('${post._id}')" class="like-button" id="${post._id}" onclick="likedOrNah('${post._id}')">❤</button>
+                    </div>
+                </div>`;
+                    postContainer.innerHTML += cardHTML;
+                } else {
+                    const cardHTML = `
+                    <div class="card text-center" id="cards" data-post-id="${post._id}">
+                    <div class="card-header">
+                    <b>@${post.username}</b>
+                    </div>
+                    <div class="card-body">
+                    <p class="card-text">${post.text}</p>
+                    </div><br>
+                    <div class="card-footer text-muted">
+                    ${convertDateTime(post.createdAt)}<br>
+                    <span class="likes-count">${countLikes(post.likes)} Likes</span>
+                    <button onmouseover="mouseOverEffect('${post._id}')" onmouseout="mouseOutEffect('${post._id}')" class="like-button liked" id="${post._id}" onclick="likedOrNah('${post._id}')">❤</button>
+                    </div>
+                    </div>`;
+                    postContainer.innerHTML += cardHTML;
+                }
             });
-
-           // attachLikeButtonEvents();
+            
         })
         .catch(error => {
             console.error(error);
         });
+    }
+    
+    window.onload = postFetch;
+    
+    function likedOrNah(postId) {
+        if (window.localStorage.getItem(postId) === null) {
+            toggleLike(postId)
+        } else {
+            untoggleLike(postId)
+        }
+    }
+    
+    function toggleLike(postId) {
+        const loginData = getLoginData();
+        const likeButton = document.querySelector(`button[id='${postId}']`)
+        likeButton.classList.toggle('liked')
+        const options = {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${loginData.token}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ postId: postId }),
+        };
+        
+        fetch(apiBaseURL + "/api/likes", options)
+        .then((response) => response.json())
+    .then((data) => {
+        window.localStorage.setItem(data.postId,data._id)
+        window.location.reload()
+    });
+    
 }
 
-postFetch();
+function untoggleLike(postId) {
+    const loginData = getLoginData();
+    const likeButton = document.querySelector(`button[id='${postId}']`)
+    likeButton.classList.toggle('liked')
+    const endpoint = window.localStorage.getItem(postId)
+    const options = {
+        method: "DELETE",
+        headers: {
+            Authorization: `Bearer ${loginData.token}`,
+            "Content-Type": "application/json",
+        },
+    };
+    fetch(apiBaseURL + "/api/likes/" + endpoint, options)
+    .then((response) => response.json())
+    .then((data) => {
+        window.localStorage.removeItem(postId)
+        window.location.reload()
+    });
+}
+
+//
+function mouseOverEffect(postId) {
+    const likeButton = document.querySelector(`button[id='${postId}']`)
+    likeButton.classList.add('mousedOver')
+}
+function mouseOutEffect(postId) {
+    const likeButton = document.querySelector(`button[id='${postId}']`)
+    likeButton.classList.remove('mousedOver')
+}
+//
+
