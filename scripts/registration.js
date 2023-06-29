dfbc5efc2f193487b79805adb99fcc6800eed175
Nerio Guio <andrik1379@gmail.com>
2023-06-29 16:00:48 -0400

    finalized registration

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,4 +1,6 @@
+
 "use strict";
+
 const fullnameInput = document.getElementById("inputName");
 const usernameInput = document.getElementById("inputUsername");
 const passwordInput = document.getElementById("InputPassword");
@@ -6,154 +8,76 @@ const confirmPasswordInput = document.getElementById("InputReEnterPassword");
 const registerBtn = document.getElementById("registerButton");
 
 window.onload = () => {
-    registerBtn.onclick = registerBtnClicked
-}
+  registerBtn.onclick = registerBtnClicked;
+};
 
-function registerBtnClicked(){
+function registerBtnClicked() {
+  let errorMessages = [];
 
-    if(passwordInput.value == confirmPasswordInput.value && usernameInput.value != "" && fullnameInput.value != "" ){
-        let bodyData = {
-            username: usernameInput.value,
-            fullName: fullnameInput.value,
-            password: passwordInput.value,
-        };
-        const options = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(bodyData),
+  if (usernameInput.value === "") {
+    errorMessages.push("Interesting, please enter a username");
+  }
+
+  if (fullnameInput.value === "") {
+    errorMessages.push("Interesting, please enter a full name");
+  }
+
+  if (passwordInput.value === "") {
+    errorMessages.push("Interesting, please enter a password");
+  }
+
+  if (confirmPasswordInput.value === "") {
+    errorMessages.push("Interesting, make sure to confirm your password");
+  }
+
+  if (errorMessages.length > 0) {
+    showErrorModal(errorMessages[0]);
+  } else if (passwordInput.value !== confirmPasswordInput.value) {
+    showErrorModal("Hmmm, looks like your passwords do not match");
+  } else {
+    let bodyData = {
+      username: usernameInput.value,
+      fullName: fullnameInput.value,
+      password: passwordInput.value,
+    };
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bodyData),
     };
     return fetch("https://microbloglite.herokuapp.com/api/users", options)
-        .then((response) => response.json())
-        .then((loginData) => {
-            window.location.assign("profile.html");
-        });
-    }
-    else{
-        console.log("error")
-        errorPass2.innerHTML = "Interesting, It seems as though you didn't fill out all the fields"
-    }
-};
-// "use strict";
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error creating user");
+        }
+        return response.json();
+      })
+      .then((loginData) => {
+        console.log(loginData);
+        window.location.assign("profile.html");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        if (error.message.includes("duplicate key")) {
+          showErrorModal("Your username has been taken");
+        } else {
+          showErrorModal("Interesting, your username has been taken");
+        }
+      });
+  }
+}
 
-// const fullnameInput = document.getElementById("inputName");
-// const usernameInput = document.getElementById("inputUsername");
-// const passwordInput = document.getElementById("InputPassword");
-// const confirmPasswordInput = document.getElementById("InputReEnterPassword");
-// const registerBtn = document.getElementById("registerButton");
-// const errorMessage = document.getElementById("errorMessage");
+function showErrorModal(errorMessage) {
+  const errorModal = new bootstrap.Modal(document.getElementById("errorModal"));
+  const errorModalMessage = document.getElementById("errorModalMessage");
+  errorModalMessage.textContent = errorMessage;
+  errorModal.show();
+}
 
-// window.onload = () => {
-//   registerBtn.onclick = registerBtnClicked;
-// };
 
-// function registerBtnClicked() {
-//   if (
-//     fullnameInput.value === "" ||
-//     usernameInput.value === "" ||
-//     passwordInput.value === "" ||
-//     confirmPasswordInput.value === ""
-//   ) {
-//     errorMessage.textContent = "Interesting, It seems as though you didn't fill out all the fields";
-//     showErrorModal();
-//     return;
-//   }
 
-//   checkUsernameAvailability(usernameInput.value)
-//     .then((isAvailable) => {
-//       if (isAvailable) {
-//         // Username is available, proceed with registration
-//         if (passwordInput.value === confirmPasswordInput.value) {
-//           createUser(usernameInput.value, fullnameInput.value, passwordInput.value)
-//             .then(() => {
-//               loginUser(usernameInput.value, passwordInput.value)
-//                 .then(() => {
-//                   window.location.assign("profile.html");
-//                 })
-//                 .catch((error) => {
-//                   console.error("Error:", error);
-//                   errorMessage.textContent = "Error logging in";
-//                   showErrorModal();
-//                 });
-//             })
-//             .catch((error) => {
-//               console.error("Error:", error);
-//               errorMessage.textContent = "Error creating user";
-//               showErrorModal();
-//             });
-//         } else {
-//           errorMessage.textContent = "Password does not match";
-//           showErrorModal();
-//         }
-//       } else {
-//         errorMessage.textContent = "Username already exists";
-//         showErrorModal();
-//       }
-//     })
-//     .catch((error) => {
-//       console.error("Error:", error);
-//       errorMessage.textContent = "Error checking username availability";
-//       showErrorModal();
-//     });
-// }
 
-// function checkUsernameAvailability(username) {
-//   const options = {
-//     method: "GET",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//   };
-
-//   return fetch("https://microbloglite.herokuapp.com/api/users", options)
-//     .then((response) => response.json())
-//     .then((users) => {
-//       const existingUser = users.find((user) => user.username === username);
-//       return !existingUser;
-//     });
-// }
-
-// function createUser(username, fullName, password) {
-//   const bodyData = {
-//     username: username,
-//     fullName: fullName,
-//     password: password,
-//   };
-
-//   const options = {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(bodyData),
-//   };
-
-//   return fetch("https://microbloglite.herokuapp.com/api/users", options)
-//     .then((response) => response.json());
-// }
-
-// function loginUser(username, password) {
-//   const bodyData = {
-//     username: username,
-//     password: password,
-//   };
-
-//   const options = {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(bodyData),
-//   };
-
-//   return fetch("https://microbloglite.herokuapp.com/api/users/login", options)
-//     .then((response) => response.json());
-// }
-
-// function showErrorModal() {
-//   const errorModal = new bootstrap.Modal(document.getElementById("errorModal"));
-//   errorModal.show();
-// }
 
 
