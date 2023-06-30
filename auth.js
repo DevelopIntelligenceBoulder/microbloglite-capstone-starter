7eb83f9c87e0922aa9fa28eaf611e503cd86748b
gioalers <101071287+gioalers@users.noreply.github.com>
2023-06-30 10:22:45 -0400

    logout fix

--- auth.js
+++ auth.js
@@ -82,6 +82,6 @@ function logout () {
             // error with the fetch request above.
 
             window.localStorage.removeItem("login-data");  // remove login data from LocalStorage
-            window.location.assign("/");  // redirect back to landing page
+            window.location.assign("../");  // redirect back to landing page
         });
 }
