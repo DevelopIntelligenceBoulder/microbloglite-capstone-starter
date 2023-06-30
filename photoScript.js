de5159eda7855e1d078d0f11ae1c1f75044d1ca6
Paris Cabrera <marvel729@gmail.com>
2023-06-30 13:55:55 -0400

    update to profile page css and html elements

--- photoScript.js
+++ photoScript.js
@@ -7,22 +7,21 @@ const imagePreview = document.getElementById("imagePreview");
 const uploadButton = document.getElementById("uploadButton");
 
 const uploadPreset = {
-  name: 'ml_default',
+  name: "ml_default",
   unsigned: false, // Set to true if you want unsigned uploads
-  apiKey: '547424269233163',
-  cloudName: 'dgfmewqop',
-  folder: 'userImages', // Optional: Specify a folder to upload the images to
-  tags: ['tag1', 'tag2'], // Optional: Add tags to the uploaded images
+  apiKey: "547424269233163",
+  cloudName: "dgfmewqop",
+  folder: "userImages", // Optional: Specify a folder to upload the images to
+  tags: ["tag1", "tag2"], // Optional: Add tags to the uploaded images
   transformations: {
     // Optional: Add desired transformations to be applied to the uploaded images
-    crop: 'limit',
+    crop: "limit",
     width: 300,
     height: 450,
+    // filename: "post-2345.jpg"
   },
 };
-console.log(uploadPreset.name)
-
-
+console.log(uploadPreset.name);
 
 // Initialize Cloudinary widget
 const myWidget = cloudinary.createUploadWidget(
@@ -30,19 +29,22 @@ const myWidget = cloudinary.createUploadWidget(
     cloudName: cloudName,
     apiKey: apiKey,
     uploadPreset: uploadPreset.name,
-    sources: ['local', 'url', 'camera'],
+    sources: ["local", "url", "camera"],
     multiple: false,
     cropping: true,
     showAdvancedOptions: true,
     maxImageFileSize: 5000000,
+    // prepareUploadParams: function (cb, params) {
+    //   cb({ public_id: post._id })
+    // },
   },
   (error, result) => {
-    if (!error && result && result.event === 'success') {
+    if (!error && result && result.event === "success") {
       // Retrieve the secure URL of the uploaded image
       const { secure_url } = result.info;
       // Display the uploaded image in the image preview
       imagePreview.src = secure_url;
-      imagePreviewContainer.style.display = "block"; 
+      imagePreviewContainer.style.display = "block";
     }
   }
 );
@@ -51,9 +53,3 @@ const myWidget = cloudinary.createUploadWidget(
 uploadButton.addEventListener("click", () => {
   myWidget.open();
 });
-
-
-
-
-
-
