f9431379bc9bf84d20d71bec091ac8a4afc9805b
davegregg@gmail.com <davegregg@gmail.com>
2023-12-20 17:43:19 -0500

    Changed apiBaseUrl to new AWS instance and added a comment pointing to the API. Dropped the Heroku mirror from the list, since it is currently down and may not be resurrected.

--- auth.js
+++ auth.js
@@ -2,8 +2,12 @@
 
 "use strict";
 
-const apiBaseURL = "https://microbloglite.herokuapp.com";
-// Backup server:   https://microbloglite.onrender.com
+const apiBaseURL = "http://microbloglite.us-east-2.elasticbeanstalk.com";
+// Backup server (mirror):   "https://microbloglite.onrender.com"
+
+// NOTE: API documentation is available at /docs 
+// For example: http://microbloglite.us-east-2.elasticbeanstalk.com/docs
+
 
 // You can use this function to get the login data of the logged-in
 // user (if any). It returns either an object including the username
