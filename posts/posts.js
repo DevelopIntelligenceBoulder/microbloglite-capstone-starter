71a45431309f93a842c63e0e64133b267591b50d
Rose Torchon <rtorchon218@gmail.com>
2023-12-21 15:54:46 -0500

    added logo created cards

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,18 @@
 /* Posts Page JavaScript */
 
 "use strict";
+
+//Prevent access to the page unless
+//the visitor is logged in
+
+//link to profile page
+
+// include logout button *function provided!
+// display all posts (display content,
+// author, timestamp) *use fetch
+
+// create vector imgs with a bud (unliked post)
+// blooming flower (liked post)
+// custom compass direction vector img (cursor)
+
+//read me explanantion!
