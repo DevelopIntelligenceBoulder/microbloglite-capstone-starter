5f90ac043233fa7c28fbb70347f949bd28c392ec
TeeeCodes <Terencepriesterr@gmail.com>
2023-12-21 14:46:08 -0500

    dropdown added

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,21 @@
 /* Posts Page JavaScript */
 
 "use strict";
+window.onload = () => {
+    function toggleDropdown() {
+        let dropdown = document.getElementById("dropdownContent");
+        dropdown.classList.toggle("show");
+    }
+
+    window.onclick = function(e) {
+        if(!e.target.matches('.nav-user-icon')) {
+            let dropdowns = document.getElementsByClassName("dropdown-content");
+            for(let i = 0; i < dropdowns.length; i++) {
+                let openDropdown = dropdowns[i];
+                if(openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+}
