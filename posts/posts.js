1b2fc8b7f57aca9932bec9acc78f610ed1366ed8
TeeeCodes <Terencepriesterr@gmail.com>
2023-12-21 15:24:49 -0500

    post box made

--- posts/posts.js
+++ posts/posts.js
@@ -1,21 +1,23 @@
 /* Posts Page JavaScript */
 
 "use strict";
-window.onload = () => {
-    function toggleDropdown() {
-        let dropdown = document.getElementById("dropdownContent");
-        dropdown.classList.toggle("show");
-    }
 
-    window.onclick = function(e) {
-        if(!e.target.matches('.nav-user-icon')) {
-            let dropdowns = document.getElementsByClassName("dropdown-content");
-            for(let i = 0; i < dropdowns.length; i++) {
-                let openDropdown = dropdowns[i];
-                if(openDropdown.classList.contains('show')) {
-                    openDropdown.classList.remove('show');
-                }
-            }
-        }
+  function toggleDropdown() {
+    let dropdown = document.getElementById("dropdownContent");
+    dropdown.classList.toggle("show");
+  }
+
+  window.onclick = function (e) {
+    if (!e.target.matches(".nav-user-icon")) {
+      let dropdown = document.getElementsByClassName("dropdown-content");
+
+      if (dropdown.classList.contains("show")) {
+        dropdown.classList.remove("show");
+      }
     }
-}
+  };
+
+  window.onload = () => {
+    
+  }
+
