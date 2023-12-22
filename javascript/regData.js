5de14a9c7f38c3270496a13d8a72d3d851847643
Shaquille Taj <staj00647@gmail.com>
2023-12-22 13:23:07 -0500

    added registration javascript

--- javascript/regData.js
+++ javascript/regData.js
@@ -35,3 +35,25 @@ regForm.onsubmit = function (event) {
 //     "count": 0,
 //     "statusCode": 399
 //   }
+function Register (regData) {
+    // POST /auth/login
+    const options = { 
+        method: "POST",
+        headers: {
+            // This header specifies the type of content we're sending.
+            // This is required for endpoints expecting us to send
+            // JSON data.
+            "Content-Type": "application/html",
+        },
+        body: JSON.stringify(regData),
+    };
+
+    return fetch(api + "/api/users", options)
+        .then(response => response.json())
+        .then(regData => {
+            window.localStorage.setItem("reg-data", JSON.stringify(regData));
+            window.location.assign("./index.html");  // redirect
+
+            return regData;
+        });
+}
