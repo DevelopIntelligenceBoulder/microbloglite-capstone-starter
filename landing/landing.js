1abf005a4b8330327bd0a44f7e7b7c89a5f0623c
Jonathan Groberg <jongroberg@gmail.com>
2023-12-22 13:26:38 -0500

    Refactor landing page HTML, CSS, and JavaScript

--- landing/landing.js
+++ landing/landing.js
@@ -1,24 +1,20 @@
-/* Landing Page JavaScript */
-
-"use strict";
-
-const loginForm = document.querySelector("#login");
-
-loginForm.onsubmit = function (event) {
-    // Prevent the form from refreshing the page,
-    // as it will do by default when the Submit event is triggered:
-    event.preventDefault();
-
-    // We can use loginForm.username (for example) to access
-    // the input element in the form which has the ID of "username".
-    const loginData = {
-        username: loginForm.username.value,
-        password: loginForm.password.value,
-    }
-
-    // Disables the button after the form has been submitted already:
-    loginForm.loginButton.disabled = true;
-
-    // Time to actually process the login using the function from auth.js!
-    login(loginData);
+window.onload = () => {
+  if (isLoggedIn() === true) window.location.replace("/posts");
+  else loginHandler();
 };
+
+function loginHandler(event) {
+  // Get and read login form
+  const loginForm = document.querySelector("#login");
+  const loginData = {
+    username: loginForm.username.value,
+    password: loginForm.password.value,
+  };
+
+  // Disable login button and process the login function in auth.js//
+  loginForm.submit = (event) => {
+    event.preventDefault();
+    loginForm.loginButton.disabled = true;
+    login(loginData);
+  };
+}
