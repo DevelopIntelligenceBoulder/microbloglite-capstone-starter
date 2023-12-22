52cbdf5cc7eb80156b71a134d49eed7f65c8e0a2
Jonathan Groberg <jongroberg@gmail.com>
2023-12-22 15:58:01 -0500

    Update authorization headers in profile.js

--- profile/profile.js
+++ profile/profile.js
@@ -1,15 +1,20 @@
 const apiUrl = "http://microbloglite.us-east-2.elasticbeanstalk.com/api/";
+const {token, username} = getLoginData();
 const options = {
   method: "GET",
-  headers: {Authorization:getLoginData().token,"Content-Type": "application/json" },
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${token}`,
+  },
 };
 
 //const token = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3RlciIsImlhdCI6MTcwMzI3MTIyNSwiZXhwIjoxNzAzMzU3NjI1fQ.HaGtg5jQeAn9hNqq8tQi0mXivLl-9w1U5YaaKA-YQqg
 
-console.log(getLoginData().token);
+console.log(username);
 
-fetch(apiUrl + "users/",options)
+fetch(apiUrl + "users/", options)
   .then((res) => res.json())
   .then((users) => {
+    console.log(getLoginData());
     console.log(users);
   });
