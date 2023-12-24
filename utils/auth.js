b83fefbb6ae452ce60899984277d0d294c569a00
Jonathan Groberg <jongroberg@gmail.com>
2023-12-23 22:54:44 -0500

    Update API URL and refactor login and logout functions

--- utils/auth.js
+++ utils/auth.js
@@ -1,82 +1,48 @@
-/* auth.js provides LOGIN-related functions */
+const apiBaseURL = "https://microbloglite.cyclic.app";
 
-"use strict";
+// Retrive login data from local storage
+let getLoginData = () => {
+  let userData = window.localStorage.getItem("user-data");
+  return JSON.parse(userData) || {};
+};
 
-const apiBaseURL = "http://microbloglite.us-east-2.elasticbeanstalk.com";
+// Checks if use login token is saved locally
+let isLoggedIn = () => "token" in getLoginData();
 
-// You can use this function to get the login data of the logged-in
-// user (if any). It returns either an object including the username
-// and token, or an empty object if the visitor is not logged in.
-function getLoginData () {
-    const loginJSON = window.localStorage.getItem("login-data");
-    return JSON.parse(loginJSON) || {};
+// POST /auth/login
+async function login(loginData) {
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(loginData),
+  };
+
+  return await fetch(apiBaseURL + "/auth/login", options)
+    .then((res) => res.json())
+    .then((userData) => {
+      window.localStorage.setItem("user-data", JSON.stringify(userData));
+      window.location.assign("/posts"); // redirect to posts
+      return userData;
+    });
 }
 
+// GET /auth/logout
+async function logout() {
+  const loginData = getLoginData();
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${loginData.token}`,
+    },
+  };
 
-// You can use this function to see whether the current visitor is
-// logged in. It returns either `true` or `false`.
-function isLoggedIn () {
-    const loginData = getLoginData();
-    return Boolean(loginData.token);
-}
-
-
-// This function is already being used in the starter code for the
-// landing page, in order to process a user's login. READ this code,
-// and feel free to re-use parts of it for other `fetch()` requests
-// you may need to write.
-function login (loginData) {
-    // POST /auth/login
-    const options = {
-        method: "POST",
-        headers: {
-            // This header specifies the type of content we're sending.
-            // This is required for endpoints expecting us to send
-            // JSON data.
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(loginData),
-    };
-
-    return fetch(apiBaseURL + "/auth/login", options)
-        .then(response => response.json())
-        .then(loginData => {
-            window.localStorage.setItem("login-data", JSON.stringify(loginData));
-            window.location.assign("/posts");  // redirect
-
-            return loginData;
-        });
-}
-
-
-// This is the `logout()` function you will use for any logout button
-// which you may include in various pages in your app. Again, READ this
-// function and you will probably want to re-use parts of it for other
-// `fetch()` requests you may need to write.
-function logout () {
-    const loginData = getLoginData();
-
-    // GET /auth/logout
-    const options = {
-        method: "GET",
-        headers: {
-            // This header is how we authenticate our user with the
-            // server for any API requests which require the user
-            // to be logged-in in order to have access.
-            // In the API docs, these endpoints display a lock icon.
-            Authorization: `Bearer ${loginData.token}`,
-        },
-    };
-
-    fetch(apiBaseURL + "/auth/logout", options)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .finally(() => {
-            // We're using `finally()` so that we will continue with the
-            // browser side of logging out (below) even if there is an
-            // error with the fetch request above.
-
-            window.localStorage.removeItem("login-data");  // remove login data from LocalStorage
-            window.location.assign("/");  // redirect back to landing page
-        });
+  return await fetch(apiBaseURL + "/auth/logout", options)
+    .then((response) => response.json())
+    .then((data) => data)
+    .finally(() => {
+      window.localStorage.removeItem("user-data");
+      window.location.assign("/"); // redirect back to landing page
+    });
 }
