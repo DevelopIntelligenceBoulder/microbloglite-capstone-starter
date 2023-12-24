8555d6f58e143c138c2232716156f25aa48ae170
Jonathan Groberg <jongroberg@gmail.com>
2023-12-23 23:28:55 -0500

    refactors login, logout to save storage to local or session based on user preference

--- utils/auth.js
+++ utils/auth.js
@@ -1,14 +1,5 @@
 const apiBaseURL = "https://microbloglite.cyclic.app";
 
-// Retrive login data from local storage
-let getLoginData = () => {
-  let userData = window.localStorage.getItem("user-data");
-  return JSON.parse(userData) || {};
-};
-
-// Checks if use login token is saved locally
-let isLoggedIn = () => "token" in getLoginData();
-
 // POST /auth/login
 async function login(loginData) {
   const options = {
@@ -22,7 +13,10 @@ async function login(loginData) {
   return await fetch(apiBaseURL + "/auth/login", options)
     .then((res) => res.json())
     .then((userData) => {
-      window.localStorage.setItem("user-data", JSON.stringify(userData));
+      // Check where to save storage and save it
+      loginData.remember
+        ? window.localStorage.setItem("user-data", JSON.stringify(userData))
+        : window.sessionStorage.setItem("user-data", JSON.stringify(userData));
       window.location.assign("/posts"); // redirect to posts
       return userData;
     });
@@ -42,7 +36,22 @@ async function logout() {
     .then((response) => response.json())
     .then((data) => data)
     .finally(() => {
+      // Clear all storage
+      window.sessionStorage.removeItem("user-data");
       window.localStorage.removeItem("user-data");
       window.location.assign("/"); // redirect back to landing page
     });
 }
+
+// Retrive login data from session storage
+let getLoginData = () => {
+  // Checks for user-data and gives empty if not found
+  let userData =
+    window.sessionStorage.getItem("user-data") ||
+    window.localStorage.getItem("user-data") ||
+    "{}";
+  return JSON.parse(userData);
+};
+
+// Checks if use login token is saved locally
+let isLoggedIn = () => "token" in getLoginData();
