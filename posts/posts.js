e79b409c86aa843ad2d9bc9c172b213543277e64
Javier Ballester <Javirb26@gmail.com>
2023-12-26 10:58:51 -0500

    WIP loading all posts and fixing login token issue

--- posts/posts.js
+++ posts/posts.js
@@ -11,24 +11,55 @@
 
 */
 "use strict";
-import {logout } from "../auth.js";
+import { isLoggedIn, getLoginData, logout } from "../auth.js";
 
 window.onload = () => {
+
+    if (!isLoggedIn()) {
+        window.location.replace("/");
+    }
+
+    getAllPosts();
     // logout button variable
     let logoutBtn = document.getElementById('logout-btn');
-    logoutBtn.onclick = () =>{
+    logoutBtn.onclick = () => {
         logout();
     }
-    // create post variables
-    let createPostForm = document.getElementById('create-post-form');
-    let postText = document.getElementById('post-text');
-    let postSubmit = document.getElementById('post-submit');
-    // display post variables
-    let allPosts = document.getElementById('all-posts');
 
-    
+
 }
 
-// let getAllPosts = () => {
-    
-// }
+// create post variables
+let createPostForm = document.getElementById('create-post-form');
+let postText = document.getElementById('post-text');
+let postSubmit = document.getElementById('post-submit');
+// display post variables
+let allPosts = document.getElementById('all-posts');
+
+let getAllPosts = () => {
+
+    const loginData = getLoginData();
+
+    const options = {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${loginData.token}`,
+        },
+    };
+
+    // apiUrl variable
+    const apiBaseURL = "http://microbloglite.us-east-2.elasticbeanstalk.com";
+
+    // fetch GET all posts
+    fetch(`${apiBaseURL}/api/posts`, options)
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error("Could not retrieve posts.");
+            }
+            return res.json();
+        })
+        .then((posts) => {
+            console.log(posts);
+        })
+        .catch((err) => console.err(err, 'Error'));
+}
