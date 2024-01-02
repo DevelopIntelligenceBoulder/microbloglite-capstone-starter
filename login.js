3ff4230a99803d88727fcea331199a393bcf907d
NPRojas <63315208+NPRojas@users.noreply.github.com>
2024-01-02 13:48:42 -0500

    add login api

---
+++ login.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const apiBaseURL = "https://microbloglite.onrender.com";
+
+function login (loginData) {
+    // POST /auth/login
+    const options = { 
+        method: "POST",
+        headers: {
+            // This header specifies the type of content we're sending.
+            // This is required for endpoints expecting us to send
+            // JSON data.
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginData),
+    };
+
+    return fetch(apiBaseURL + "/auth/login", options)
+        .then(response => response.json())
+        .then(loginData => {
+            window.localStorage.setItem("login-data", JSON.stringify(loginData));
+            //window.location.assign("/posts");  // redirect
+            console.log(loginData);
+            return loginData;
+        });
+}
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // get username and password from the form
+    const loginData = {
+        username: document.getElementById("username").value,
+        password: document.getElementById("password").value
+    };
+
+    // make an API request to login
+    login(loginData);
+});
