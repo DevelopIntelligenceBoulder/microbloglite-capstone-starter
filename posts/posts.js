a99707f81642639211c6d27035ac1e543e9ebd54
Alexis Campos <Alexisxcampos@gmail.com>
2024-01-02 10:30:25 -0500

    LOGOUT BUTTON MWAHAHA

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,14 @@
 /* Posts Page JavaScript */
 
 "use strict";
+
+// Function to handle logout
+function logout() {
+    localStorage.clear();
+    sessionStorage.clear();
+    window.location.href = "/";
+}
+document.getElementById("logoutLink").addEventListener("click", (event) => {
+    event.preventDefault();
+    logout();
+});
