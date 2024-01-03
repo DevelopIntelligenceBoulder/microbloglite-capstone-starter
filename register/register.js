1c830f865fe02fbdf0ae7a0aa48fe34d61afa3e9
Alpha Diallo <ssheppard@myemailprovider.com>
2024-01-03 15:46:10 -0500

    Completed Registration Page

--- register/register.js
+++ register/register.js
@@ -2,30 +2,35 @@
 
 const registrationForm = document.querySelector('#register');
 
-registrationForm.onsubmit = function (event) {
+registrationForm.onsubmit = DOSOMETHING
 
-    event.preventDefault();
+function DOSOMETHING (event) {
+    event.preventDefault()
+    console.log("HEYAAA")
 
     const registerData = {
-        username: registrationForm.username.value,
-        password: registrationForm.password.value,
-        fullName: registrationForm.fullName.value
+        username: registrationForm.elements.username.value,
+        password: registrationForm.elements.password.value,
+        fullName: registrationForm.elements.fullName.value
     };
+    return Post(registerData)
+}
 
-    register(registerData);
-
-
-fetch('http://microbloglite.us-east-2.elasticbeanstalk.com/api/users', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'},
-    body: JSON.stringify(registerData) 
-})
-.then(response => response.json())
-.then(data => {
-    console.log('Success:', data);
-})
-.catch((error) => {
-    console.error('Error:', error);
-});
+function Post (registerData) {
+    fetch('http://microbloglite.us-east-2.elasticbeanstalk.com/api/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(registerData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            window.location.assign("/");
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 };
+
