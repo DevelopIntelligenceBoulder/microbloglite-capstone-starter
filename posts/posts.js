04a77f0d25767e428fd8ec475ddb5a2bf670a6d6
Needy Coder <xabierjones5749@gmail.com>
2024-01-03 15:31:12 -0500

    fetching

--- posts/posts.js
+++ posts/posts.js
@@ -1,5 +1,21 @@
 /* Posts Page JavaScript */
-let logoutBtn= 
-document.getElementById("Logout-button");
-logoutBtn.onclick= logout;
+let logoutBtn =
+    document.getElementById("Logout-button");
+logoutBtn.onclick = logout;
 "use strict";
+
+
+const loginData = getLocalUserData();
+const options = {
+    method: "GET",
+    headers: {
+        Authorization: `Bearer ${loginData.token}`,
+    },
+};
+
+return await fetch(apiBaseURL + "/api/posts", options)
+    .then((response) => response.json())
+    .then((posts) => {
+        console.log(posts); //log grab all post 
+
+    });
