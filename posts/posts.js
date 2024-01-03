5d2bd29f82b23319f8699e2534af3d614f8796d9
Juan11211 <avilesjuan628@gmail.com>
2024-01-03 09:31:50 -0500

    fixed some bugs in the post page

--- posts/posts.js
+++ posts/posts.js
@@ -4,35 +4,27 @@ let userData;
 
 window.onload = () => {
     fetchAllPosts();
-
 };
 
 function fetchAllPosts() {
     const token = getLoginData().token;
 
-
     fetch('http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts', {
         method: "GET",
         headers: {
-
             Authorization: `Bearer ${token}`
-
         }
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Failed to fetch posts');
-        }
-        return response.json();
-    })
+    .then(response => response.json())
     .then(allPosts => {
         console.log(allPosts);
         displayAllPosts(allPosts);
     })
     .catch(error => {
-        console.error("Failed to fetch posts:", error);
+        console.error("Failed to fetch posts", error);
     });
 }
+
 
 function likePost(postId) {
     console.log(`Liking post with ID: ${postId}`);
@@ -47,55 +39,89 @@ function likePost(postId) {
         headers: {
             "Content-Type": "application/json",
             "Accept": "application/json",
-
             Authorization: `Bearer ${token}`
-
         },
         body: JSON.stringify(bodyObject)
     })
     .then(response => {
-        if (!response.ok) {
-            throw new Error('Failed to like the post');
-        }
-        return response.json();
+         response.json();
     })
     .then(likedPost => {
         console.log(likedPost);
         fetchAllPosts();
-
     })
     .catch(error => {
         console.error("Failed to like the post:", error);
 
     });
 }
+//     const token = getLoginData().token;
+
+//     const apiUrl = `http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts/${postId}`;
+//     fetch(apiUrl, {
+//         method: 'GET',
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json",
+//             "Authorization": `Bearer ${token}`
+//         }
+//     })
+//         .then(response => response.json())
+//         .then(deletedPost => {
+//             console.log(`Post with ID ${postId} deleted successfully.`, deletedPost);
+//             return fetchAllPosts();
+//         })
+//         .then(response => response.json())
+//         .then(postData => {
+//             if (getLoginData().username === postData.username) {
+//                 // If yes, proceed with the delete request
+//                 return fetch(apiUrl, {
+//                     method: 'DELETE',
+//                     headers: {
+//                         'Content-Type': 'application/json',
+//                         'Accept': 'application/json',
+//                         Authorization: `Bearer ${token}`
+//                     }
+//                 });
+//             } else {
+//                 const card = document.getElementById(`postCard_${postId}`);
+//                 if (card) {
+//                     const errMsg = document.createElement('p');
+//                     errMsg.innerHTML = "You don't have permission to delete this post.";
+//                     errMsg.style.color = "red";
+//                     card.appendChild(errMsg);
+//                 }
+//                 // Throw an error to prevent card removal
+//                 throw new Error("Permission denied");
+//             }
+//         })
+//         .then(() => {
+//             // IF POST WAS DELETED, THIS WILL RUN 
+//             const card = document.getElementById(`postCard_${postId}`);
+//             if (card) {
+//                 card.remove();
+//             }
+//         })
+//         .catch(error => {
+//             console.error('Error deleting post', error);
+//         });
+//     }    
 
 function deletePost(postId) {
-
     const token = getLoginData().token;
-
     const apiUrl = `http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts/${postId}`;
 
     // Fetch post data to check for the creator
     fetch(apiUrl, {
         method: 'GET',
         headers: {
-
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Authorization": `Bearer ${userData.token}`
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            Authorization: `Bearer ${token}`
         }
     })
     .then(response => response.json())
-    .then(deletedPost => {
-        console.log(`Post with ID ${postId} deleted successfully.`, deletedPost);
-        fetchAllPosts();
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            "Authorization": `Bearer ${token}`
-        }
-    });
-    .then(response => response.json())
+    // CHECKING TO SEE IF LOGGED IN USERNAME MATCHES THE PERSON WHO CREATED IT
     .then(postData => {
         if (getLoginData().username === postData.username) {
             // If yes, proceed with the delete request
@@ -115,12 +141,12 @@ function deletePost(postId) {
                 errMsg.style.color = "red";
                 card.appendChild(errMsg);
             }
-            // Throw an error to prevent card removal
+            // THIS WILL STOP CODE FROM CONTINUING AND MOVE ON TO THE CATCH ERROR
             throw new Error("Permission denied");
         }
     })
     .then(() => {
-        // This block will only be executed if the post deletion was successful
+        // IF POST CAN DELETE, THIS WILL RUN 
         const card = document.getElementById(`postCard_${postId}`);
         if (card) {
             card.remove();
@@ -135,7 +161,7 @@ function displayAllPosts(allPosts) {
     let allPostContainer = document.getElementById("allPostContainer");
     allPostContainer.innerHTML = ""; 
   
-    allPosts.forEach(post => {
+    allPosts?.forEach(post => {
        
         const card = document.createElement('div');
         card.className = 'card mb-3';
@@ -151,10 +177,8 @@ function displayAllPosts(allPosts) {
             <button class="btn btn-dark text-light" onclick="deletePost('${post._id}')">Delete Post</button>
         `;
 
-        // Append card body to the card
         card.appendChild(cardBody);
-
-        // Set card ID and append to the container
+        // giving card id value -- with this we're able to do getElementById. 
         card.id = `postCard_${post._id}`;
         allPostContainer.appendChild(card);
     });
