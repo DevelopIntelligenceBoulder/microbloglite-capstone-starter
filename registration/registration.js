dd58e03dc55c0ad826294cec3e1b347d2374b632
ltejedajr <126844440+ltejedajr@users.noreply.github.com>
2024-01-03 01:24:32 -0500

    Modified reg script, html, and added to global css for synchronicity

--- registration/registration.js
+++ registration/registration.js
@@ -1,7 +1,6 @@
 "use strict";
 
-const firstNameInput = document.getElementById('firstName');
-const lastNameInput = document.getElementById('lastName');
+const fullNameInput = document.getElementById('fullName');
 const userNameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
 const bioTextInput = document.getElementById('bioText');
@@ -9,30 +8,39 @@ const newUserBtn = document.getElementById('newUser');
 
 window.onload = init;
 
-function init (){
-    newUserBtn.onclick = newUserBtnClicked ;
-    
-  }
+function init() {
+    newUserBtn.onclick = newUserBtnClicked;
+}
 
-  function newUserBtnClicked (){
-    
-      fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/users", {
+function newUserBtnClicked() {
+    const newUserData = {
+        fullName: fullNameInput.value,
+        username: userNameInput.value,
+        password: passwordInput.value,
+        bioText: bioTextInput.value
+    };
+
+    // Make a request to the server to register the new user
+    fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/users", {
         method: "POST",
-        body: JSON.stringify({
-          username: userNameInput.value,
-          fullName: "firstNameInput.value" + "" + "lastNameInput.value",
-          password: passwordInput.value,
-          }),
-        headers: { "Content-type": "application/json;charset=UTF-8" },
-      })
-      .then(response => response.json())
-      
-      .then(json =>{
-        console.log(json);
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newUserData)
+    })
+    .then(response => response.json())
+    .then(data => {
+
+        const userToken = data.token;
+
+        // Store the token in the local storage
+        localStorage.setItem("userToken", userToken);
+
+        // Redirect to the landing page to login
         window.location.replace('/landing/landing.html');
-      })
-      .catch(error => {
+    })
+    .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
         // Handle the error appropriately
-      });
-    }
+    });
+}
