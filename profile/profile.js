bd9e4148bb3a1662b873f80493cac9c29d2016eb
konate00 <okonate0913@gmail.com>
2024-01-03 11:54:54 -0500

    editUser function added

--- profile/profile.js
+++ profile/profile.js
@@ -1,12 +1,12 @@
 
 "use strict"
 
-
+const profileContainer = document.getElementById('profile');
 window.onload = function() {
     const postbtn = document.querySelector('#postBtn');
     postbtn.onclick = addPost;
 
-    const profileContainer = document.getElementById('profile');
+    
     const userData = getLoginData();
 
     if (userData.username) {
@@ -29,9 +29,9 @@ function addPost() {
         method: 'POST', 
         body: JSON.stringify(bodyData),
         headers: {'Content-Type': 'application/json',
-    "Accept": "application/json",
-                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBvcHNtb2tlIiwiaWF0IjoxNzA0MjI0NzczLCJleHAiOjE3MDQzMTExNzN9.v478-orrt1_zfrZTX4hHK3a99zP9Un5CJQemW6xqddQ"
-                //Authorization: `${userData.token}`
+                'Accept': 'application/json',
+                // "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imp1YW4xMjMiLCJpYXQiOjE3MDQyOTAzMzUsImV4cCI6MTcwNDM3NjczNX0.QHXBPnUc1KFZ7kJwknWTcj8XuqPqqMPRcfaAmnjwgn0"
+                // 'Authorization': `Bearer ${userData.token}`
             }
     })
     .then(response => response.json())
@@ -40,10 +40,65 @@ function addPost() {
 
 }
 
-function editUser(userInfo){
+function editUser(){
 
+    const usernameEl = document.querySelector('h2');
+
+    const newUsername = prompt('Enter you new username: ', usernameEl.innerText);
     
+    if (newUsername !== null && newUsername.trim() !== ''){
+        usernameEl.innerText = newUsername;
+
+        const userData = getLoginData();
+
+        const currentUsername = userData.username;
+
+        fetch(`http://microbloglite.us-east-2.elasticbeanstalk.com/api/users/${currentUsername}`, {
+            method: 'PUT',
+            body: JSON.stringify({ username: newUsername}),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${userData.token}`
+                }
+        })
+        .then(res => res.json())
+        .then(updatedUserData => {
+            // DISPLAY UPDATED USER
+            console.log(updatedUserData)})
+            displayProfile(updatedUserData)
+        .catch((err) => console.error('Error update username:', err));
+
+    }else{
+        alert("Please enter a valid username");
+    }
 
 }
 
+function displayProfile(updatedUserData){
+    
+    // go into updatedUserData retrive username = pdatedUserData.username 
+
+    // make a fetch request with username just like the put u did - GET REQUEST TO GET UODATED USERNAME 
+
+    const usernameEl = document.querySelector('h2');
+    usernameEl.innerHTML = `${updatedUserData.username}`;
+
+    const userData = getLoginData();
+
+    fetch(`http://microbloglite.us-east-2.elasticbeanstalk.com/api/users/${updatedUserData.username}`, {
+        method:'GET',
+        headers:{
+            "Content-Type":'application/json',
+            'Accept': 'application/json',
+            'Authorization':`Bearer ${userData.token}`
+        }
+    })
+    .then(resp=> resp.json())
+    .then(updatedUserData => {
+        console.log('Updated User Data:', updatedUserData);
+    })
+    .catch(err => console.error('Error fetching updated user data:', err))
+    
+}
 
