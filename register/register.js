dbb63d84aa9981c01fd6ceb92bec687b2f93c6f2
Alpha Diallo <ssheppard@myemailprovider.com>
2024-01-03 13:15:24 -0500

    main

--- register/register.js
+++ register/register.js
@@ -1,35 +1,31 @@
-'use strict'
+'use strict';
 
-const registrationForm = document.querySelector(Register);
+const registrationForm = document.querySelector('#register');
+
 registrationForm.onsubmit = function (event) {
-    console.log("hello world")
-    // Prevent the form from refreshing the page,
-    // as it will do by default when the Submit event is triggered:
+
     event.preventDefault();
 
-const RegisterData = {
-    username: registrationForm.username.value,
-    password: registrationForm.password.value,
-    fullName: registrationForm.fullName.value
-}
+    const registerData = {
+        username: registrationForm.username.value,
+        password: registrationForm.password.value,
+        fullName: registrationForm.fullName.value
+    };
+
+    register(registerData);
 
 
-    // Time to actually process the login using the function from auth.js!
-    Register(RegisterData);
-}
-    fetch('http://microbloglite.us-east-2.elasticbeanstalk.com/api/users', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(users)
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-    
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    }
-    )
+fetch('http://microbloglite.us-east-2.elasticbeanstalk.com/api/users', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'},
+    body: JSON.stringify(registerData) 
+})
+.then(response => response.json())
+.then(data => {
+    console.log('Success:', data);
+})
+.catch((error) => {
+    console.error('Error:', error);
+});
+};
