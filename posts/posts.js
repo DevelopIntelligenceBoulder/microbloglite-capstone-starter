dba882aa3301a3446d65d4dc6f3db0e5bbe1ea81
MWorthy@appdev.yearup.org <MWorthy@appdev.yearup.org>
2024-01-03 16:01:04 -0500

    Post page progress made! Hurrah, buddy.

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,18 @@
-/* Posts Page JavaScript */
-
-"use strict";
+// Function to get all users via fetch()
+function getAllUsers() {
+    // GET /api/users
+    const loginData = getLoginData();
+    const options = {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${loginData.token}`,
+        },
+    };
+    // note:the api variable is defined in auth.js
+    fetch(api + "/api/users", options)
+        .then(response => response.json())
+        .then(users => {
+            // Do something with the users array...
+            console.log(users);
+        });
+}
