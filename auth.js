8f15c9b6f434769a1df6e852e5cbe791ba1e9f23
Shaquille Taj <staj00647@gmail.com>
2024-01-04 15:17:37 -0500

    commented out landing reg posts and auth

--- auth.js
+++ auth.js
@@ -1,74 +1,53 @@
 /* auth.js provides LOGIN-related functions */
 
 "use strict";
-
+//api that we will call upon
 const apiBaseURL = "http://microbloglite.us-east-2.elasticbeanstalk.com";
-// Backup server (mirror):   "https://microbloglite.onrender.com"
 
-// NOTE: API documentation is available at /docs 
-// For example: http://microbloglite.us-east-2.elasticbeanstalk.com/docs
-
-
-// You can use this function to get the login data of the logged-in
-// user (if any). It returns either an object including the username
-// and token, or an empty object if the visitor is not logged in.
+//pulling our login data from our local storage
 function getLoginData () {
     const loginJSON = window.localStorage.getItem("login-data");
     return JSON.parse(loginJSON) || {};
 }
 
 
-// You can use this function to see whether the current visitor is
-// logged in. It returns either `true` or `false`.
+// this function is to see whether the current visitor is logged in. It returns either `true` or `false` using a boolean.
 function isLoggedIn () {
     const loginData = getLoginData();
     return Boolean(loginData.token);
 }
 
 
-// This function is already being used in the starter code for the
-// landing page, in order to process a user's login. READ this code,
-// and feel free to re-use parts of it for other `fetch()` requests
-// you may need to write.
+// post method to auth/login with specified content type
 function login (loginData) {
-    // POST /auth/login
     const options = { 
         method: "POST",
         headers: {
-            // This header specifies the type of content we're sending.
-            // This is required for endpoints expecting us to send
-            // JSON data.
             "Content-Type": "application/json",
         },
         body: JSON.stringify(loginData),
     };
-
+// redirect to landing page
     return fetch(apiBaseURL + "/auth/login", options)
         .then(response => response.json())
         .then(loginData => {
             window.localStorage.setItem("login-data", JSON.stringify(loginData));
-            window.location.assign("/posts");  // redirect
+            window.location.assign("/posts");  
 
             return loginData;
         });
 }
 
 
-// This is the `logout()` function you will use for any logout button
-// which you may include in various pages in your app. Again, READ this
-// function and you will probably want to re-use parts of it for other
-// `fetch()` requests you may need to write.
+// initiating the logout function
 function logout () {
     const loginData = getLoginData();
 
-    // GET /auth/logout
+    // GET method 
     const options = { 
         method: "GET",
         headers: { 
-            // This header is how we authenticate our user with the
-            // server for any API requests which require the user
-            // to be logged-in in order to have access.
-            // In the API docs, these endpoints display a lock icon.
+            // This header is how we authenticate our user with the api
             Authorization: `Bearer ${loginData.token}`,
         },
     };
@@ -77,9 +56,7 @@ function logout () {
         .then(response => response.json())
         .then(data => console.log(data))
         .finally(() => {
-            // We're using `finally()` so that we will continue with the
-            // browser side of logging out (below) even if there is an 
-            // error with the fetch request above.
+            //finally completes logout no matter the response
 
             window.localStorage.removeItem("login-data");  // remove login data from LocalStorage
             window.location.assign("/");  // redirect back to landing page
--- landing/landing.js
+++ landing/landing.js
@@ -1,17 +1,14 @@
 "use strict";
-
-//login API
-// const apiBaseURL = "microbloglite.us-east-2.elasticbeanstalk.com";
-
+//submit button element and login query
 const loginButton = document.getElementById("loginButton")
 const loginForm = document.querySelector("#login");
 
 window.onload = init;
-
+//initializing onclick event and setting a variable
 function init() {
     loginButton.onclick = loginButtonClicked;
 }
-
+//using function to set inputs as logindata constant
 function loginButtonClicked() {
     const usernameField = document.getElementById("username");
     const passwordField = document.getElementById("password");
@@ -23,49 +20,42 @@ function loginButtonClicked() {
     // Disables the button after the form has been submitted already:
     loginForm.loginButton.disabled = true;
 
-    // Time to actually process the login using the function from auth.js!
+    //process the login using the function from auth.js!
     login(loginData);
 }
 
-
+//pulling logindata from local storage
 function getLoginData() {
     const loginJSON = window.localStorage.getItem("login-data");
     return JSON.parse(loginJSON) || {};
 }
 
 
-// You can use this function to see whether the current visitor is
-// logged in. It returns either `true` or `false`.
+// boolean to see if token is in logindata
 function isLoggedIn() {
     const loginData = getLoginData();
     return Boolean(loginData.token);
 }
 
 
-// This function is already being used in the starter code for the
-// landing page, in order to process a user's login. READ this code,
-// and feel free to re-use parts of it for other `fetch()` requests
-// you may need to write.
+// information being sent through post method to api auth/login
 function login(loginData) {
-    // POST /auth/login
+ 
     const options = {
         method: "POST",
         headers: {
-            // This header specifies the type of content we're sending.
-            // This is required for endpoints expecting us to send
-            // JSON data.
             "Content-Type": "application/json",
         },
         body: JSON.stringify(loginData),
 
     };
-
+//saving response to local storage and redirect to post using fetch function
     return fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/auth/login", options)
         .then(response => response.json())
         .then(loginData => {
             console.log("loginsuccessful:",loginData)
             window.localStorage.setItem("login-data", JSON.stringify(loginData));
-            window.location.assign("/posts");  // redirect
+            window.location.assign("/posts");
 
             return loginData;
         });
