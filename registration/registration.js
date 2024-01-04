1c08b625e80515d058269e996b98490948aedd50
Jonathan Groberg <jongroberg@gmail.com>
2024-01-04 15:58:49 -0500

    Fix formatting issues in global styles and profile page

--- registration/registration.js
+++ registration/registration.js
@@ -6,13 +6,13 @@ window.onload = ()=>{
             "Content-Type": "application/json",
           },
           body: JSON.stringify(registerData),
-          
+
         };
         const res = await fetch(apiBaseURL + "/api/users", options);
         const success = await res.json();
         console.log(res.status);
         if (res.status === 201) {
-            window.location.replace("/posts/index.html"); // redirect 
+            window.location.replace("/posts/index.html"); // redirect
         } else {
             console.error("registration unsuccessful");
         }
