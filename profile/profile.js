bb2e7ce9423930d678e517356822767b6418c042
thekianajones@outlook.com <thekianajones@outlook.com>
2024-01-04 11:45:52 -0500

    Finished profile page

--- profile/profile.js
+++ profile/profile.js
@@ -1,81 +1,73 @@
 "use strict";
 
-// Imported functions
-import { isLoggedIn, getLoginData, logout } from "/auth.js";
-
 window.onload = () => {
-
-    // If the user is not logged in, redirect to the home page
-    if (!isLoggedIn()) {
-        window.location.replace("/index.html");
-    }
 
     // Function call for getAllPosts
     // getAllPosts();
 
     // Logout button variable
     let logoutBtn = document.getElementById('logout');
-    // Event listener for the logout button
-    logoutBtn.onclick = () => {
-        logout();
-    };
-
+    logoutBtn.onclick = logout
+    // Initialized variables
+    const createPostForm = document.getElementById('create-post-form');
+    // Event handler for creating a new post
+    createPostForm.onsubmit = handleSubmit
 };
 
-// Initialized variables
-const apiBaseURL = "http://microbloglite.us-east-2.elasticbeanstalk.com/";
-const createPostForm = document.getElementById('create-post-form');
 
-// Event handler for creating a new post
-createPostForm.onsubmit = (e) => {
+function handleSubmit(e) {
     e.preventDefault();
     let postText = document.getElementById('post-text').value;
+    const createPostForm = document.getElementById('create-post-form');
 
-    let postDataContent = {'text': postText};
+    let postDataContent = { 'text': postText };
 
-    const loginData = getLoginData();
+    let {token} = getLoginData();
 
     const options = {
         method: 'POST',
         headers: {
             "Content-Type": "application/json",
-            Authorization: Bearer ${loginData.token},
+            Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify(postDataContent)
-    };
+    }
 
-    fetch(${apiBaseURL}/api/posts, options)
-    .then((res) => res.json())
-    .then((newPostData) => {
+    fetch(`${apiBaseURL}/api/posts`, options)
+        .then((res) => res.json())
+        .then((_newPostData) => {
+            // display a success message
 
-        // Formats date into: (MON DD, YYYY, 0:00 AM/PM)
-        let date = new Date(newPostData.createdAt);
-        let options = {
-            hour: 'numeric',
-            minute: '2-digit',
-            hour12: true,
-            month: 'short',
-            day: 'numeric',
-            year: 'numeric',
-        };
-        let formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
+            createPostForm.reset();
+            setTimeout(() => window.location.assign("/posts"), 5000)
+        })
+        .catch((err) => console.error(err));
 
-        let newPost = document.createElement('div');
-        newPost.classList.add('container', 'd-flex', 'justify-content-center');
+}
+//         // Formats date into: (MON DD, YYYY, 0:00 AM/PM)
+//         let date = new Date(newPostData.createdAt);
+//         let options = {
+//             hour: 'numeric',
+//             minute: '2-digit',
+//             hour12: true,
+//             month: 'short',
+//             day: 'numeric',
+//             year: 'numeric',
+//         };
+//         let formattedDate = new Intl.DateTimeFormat('en-US', options).format(date);
 
-        newPost.innerHTML = 
-        <div class="card" style='width: 75%;'>
-            <div class="card-body bg-dark text-white">
-                <p class="card-title">${newPostData.username}</p>
-                <p class="card-text">${newPostData.text}</p>
-                <p class="card-text fw-lighter">${formattedDate}</p>
-            </div>
-        </div>;
-        // allPosts.prepend(newPost);
-        console.log(newPostData);
+//         let newPost = document.createElement('div');
+//         newPost.classList.add('container', 'd-flex', 'justify-content-center');
 
-    })
-    .catch((err) => console.error(err));
+//         newPost.innerHTML = `
+//             <div class="card" style='width: 75%;'>
+//                 <div class="card-body bg-dark text-white">
+//                     <p class="card-title">${newPostData.username}</p>
+//                     <p class="card-text">${newPostData.text}</p>
+//                     <p class="card-text fw-lighter">${formattedDate}</p>
+//                 </div>
+//             </div>;
+//     // allPosts.prepend(newPost);
+//     console.log(newPostData);
 
-    createPostForm.reset();
-};
+// `}
