b78875cd97a675e9fb00b0d02e4341506e53668f
Needy Coder <xabierjones5749@gmail.com>
2024-01-04 10:45:39 -0500

    Posts Page update

--- posts/posts.js
+++ posts/posts.js
@@ -1,21 +1,28 @@
 /* Posts Page JavaScript */
-let logoutBtn =
-    document.getElementById("Logout-button");
-logoutBtn.onclick = logout;
 "use strict";
+window.onload = () => {
+    let logoutBtn = document.getElementById("logout-button");
+
+    logoutBtn.onclick = logout;
 
 
-const loginData = getLocalUserData();
-const options = {
-    method: "GET",
-    headers: {
-        Authorization: `Bearer ${loginData.token}`,
-    },
-};
+    const loginData = getLocalUserData();
+    const options = {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${loginData.token}`,
+        },
+    };
 
-return await fetch(apiBaseURL + "/api/posts", options)
-    .then((response) => response.json())
-    .then((posts) => {
-        console.log(posts); //log grab all post 
-
-    });
+    fetch(apiBaseURL + "/api/posts", options)
+        .then((response) => response.json())
+        .then((posts) => {
+            console.log(posts); //log grab all post 
+            let postsContainer = document.getElementById("posts");
+            for (let post of posts) {
+                let div = document.createElement("div");
+                div.innerHTML = `<p>${post.username} ; ${post.text}</p>`
+                postsContainer.appendChild(div);
+            }
+        });
+}
