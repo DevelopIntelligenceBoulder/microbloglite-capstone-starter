369a596c916a179b45f62e370d68080350ee325e
Jayla Young <jaylayoung1@gmail.com>
2024-01-04 14:41:14 -0500

    registration page functional, photos uploaded

--- register.js
+++ register.js
@@ -1,41 +1,35 @@
+"use strict";
 // const apiBaseURL = "http://microbloglite.us-east-2.elasticbeanstalk.com";
 
-// You can use this function to get the login data of the logged-in user (if any). It returns either an object including the username and token, or an empty object if the visitor is not logged in.
-function getRegisterData () {
-    const registerJSON = window.localStorage.getItem("register-data");
-    return JSON.parse(registerJSON) || {};
-}
+const registerForm = document.querySelector("#register");
+registerForm.onsubmit = registerUser;
 
-// You can use this function to see whether the current visitor is logged in. It returns either `true` or `false`.
-function isRegistered () {
-    const registerData = getRegisterData();
-    return Boolean(registerData.token);
-}
+function registerUser(event) {
+  event.preventDefault();
+  let registerData = {
+    username: document.getElementById("username").value,
+    fullName: document.getElementById("fullName").value,
+    password: document.getElementById("password").value,
+  };
 
-let registerData = {
-  username: document.getElementById("username").value,
-  fullName: document.getElementById("fullName").value,
-  password: document.getElementById("password").value
-}
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(registerData),
+  };
 
-function registerUser() {
-   const options = { 
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(registerData),
-      };
-      
-      let registrationComplete = document.getElementById("registration-confirmation")
-      
-      fetch(apiBaseURL + "/api/users", options, {
-        method: "POST",
-        body: JSON.stringify.apply(registerData),
-        headers: {"Content-type": "application/json; charset=UTF-8"}
-      })
-      .then(response => response.json())
-      .then(json => {
-        registrationComplete.innerHTML = "You have successfully created an account."
-        
-      });
-    }
-    registerUser();
+  let registrationComplete = document.getElementById(
+    "registration-confirmation"
+  );
+
+  fetch(apiBaseURL + "/api/users", options)
+    .then((response) => response.json())
+    .then((user) => {
+      console.log("am I running?");
+      registrationComplete.innerHTML = `
+        <p>You have successfully created an account. 
+        <a href="/">Click here to login.</a>
+      `;
+      // window.location.assign("/")
+    });
+}
