2810fd6bd1de095a04be1ae2e5a895ce7c82280e
dara.fuller16@yahoo.com <dara.fuller16@yahoo.com>
2024-01-05 14:25:06 -0500

    WIP login page

--- landing.js
+++ landing.js
@@ -8,16 +8,17 @@ loginForm.onsubmit = function (event) {
     // Prevent the form from refreshing the page,
     // as it will do by default when the Submit event is triggered:
     event.preventDefault();
-
+    
     // We can use loginForm.username (for example) to access
     // the input element in the form which has the ID of "username".
     const loginData = {
         username: loginForm.username.value,
         password: loginForm.password.value,
     }
+    console.log(loginData)
 
     // Disables the button after the form has been submitted already:
-    loginForm.loginButton.disabled = true;
+    loginForm.querySelector("#loginButton").disabled = true;
 
     // Time to actually process the login using the function from auth.js!
     login(loginData);
