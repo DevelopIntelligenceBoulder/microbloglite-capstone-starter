b73ad93224f86045ef3f5955324a968a38a6a844
Jonathan Groberg <jongroberg@gmail.com>
2024-01-04 22:16:07 -0500

    Refactor navigation links and update post cards

--- posts/posts.js
+++ posts/posts.js
@@ -1,38 +1,36 @@
 /* Posts Page JavaScript */
 "use strict";
 window.onload = () => {
-    let logoutBtn = document.getElementById("logoutBtn");
+  let logoutBtn = document.getElementById("logoutBtn");
 
-    logoutBtn.onclick = logout;
+  logoutBtn.onclick = logout;
 
+  const loginData = getLocalUserData();
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${loginData.token}`,
+    },
+  };
 
-    const loginData = getLocalUserData();
-    const options = {
-        method: "GET",
-        headers: {
-            Authorization: `Bearer ${loginData.token}`,
-        },
-    };
-
-    fetch(apiBaseURL + "/api/posts", options)
-        .then((response) => response.json())
-        .then((posts) => {
-            console.log(posts); //log grab all post 
-            let postsContainer = document.getElementById("posts");
-            for (let post of posts) {
-                let div = document.createElement("div");
-                div.innerHTML = `<div class="card justify-content-center " style="width:18 rem;">
+  fetch(API_URL + "/api/posts", options)
+    .then((response) => response.json())
+    .then((posts) => {
+      console.log(posts); //log grab all post
+      let postsContainer = document.getElementById("posts");
+      for (let post of posts) {
+        let div = document.createElement("div");
+        div.innerHTML = `<div class="card justify-content-center " style="width:18 rem;">
   <div class="card-body ">
     <h5 class="card-title">${post.username}</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">Users Mood</h6>
     <p class="card-text" >${post.text}</p>
-    <a href="#" class="card-link">Love it</a>
-    <a href="#" class="card-link">Fudge it</a>
+    <a class="btn text-primary">Love it</a>
+    <a class="btn text-danger-emphasis">Fudge it</a>
 
   </div>
 </div>
- </p>`
-                postsContainer.appendChild(div);
-            }
-        });
-}
+ </p>`;
+        postsContainer.appendChild(div);
+      }
+    });
+};
