d0ed649a39ebcec1f8dd010782c071f87c5c9dc2
Alec Philavong <alecphil@live.com>
2024-06-24 13:50:31 -0500

    add functionality to register page

--- posts/posts.js
+++ posts/posts.js
@@ -1,3 +1,55 @@
 /* Posts Page JavaScript */
 
 "use strict";
+
+window.onload = () => {
+
+    getPosts();
+
+    getPostsAsyncExample();
+
+}
+
+
+const getPosts = () => {
+
+    const loginData = getLoginData();
+
+    fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts", {
+        method: "GET",
+        headers: {
+            // This header is how we authenticate our user with the
+            // server for any API requests which require the user
+            // to be logged-in in order to have access.
+            // In the API docs, these endpoints display a lock icon.
+            Authorization: `Bearer ${loginData.token}`
+        }
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            //do something with the posts
+            console.log(data, "Using Promises")
+        })
+
+}
+
+const getPostsAsyncExample = async () => {
+
+    const loginData = getLoginData();
+
+    const response = await fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts", {
+        method: "GET",
+        headers: {
+            // This header is how we authenticate our user with the
+            // server for any API requests which require the user
+            // to be logged-in in order to have access.
+            // In the API docs, these endpoints display a lock icon.
+            Authorization: `Bearer ${loginData.token}`
+        }
+    })
+
+    const data = await response.json();
+
+    //do something with the posts
+    console.log(data, "using async/await")
+}
---
+++ registration_page/register.js
@@ -0,0 +1,52 @@
+"use strict"
+
+window.onload = () => {
+
+
+    const registerform = document.querySelector("#registerForm")
+
+    registerform.addEventListener("submit", signUp)
+}
+
+//method/function to sign up form 
+//CRUD: (C)reate a user
+const signUp = async (event) => {
+
+    //call preventDefault to keep the page from reloading
+    event.preventDefault();
+
+    //generate a new form data object
+    let formData = new FormData(event.target);
+
+    //generate a JavaScript Object from the formData object created above
+    let formDataAsObject = Object.fromEntries(formData);
+
+    //try catch for error handling
+    try {
+
+        //make a fetch (POST) request to create a comment in the API
+        let response = await fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/users",
+            {
+                method: "POST",
+                headers: { "Content-type": "application/json; charset=UTF-8" },
+                //take the data from the form and build the body of the request
+                body: JSON.stringify(formDataAsObject)
+            }
+        );
+        //turn the response in to something we can work with
+        let newCourse = await response.json();
+
+        //put the comments in the console
+        console.log(newCourse, "this should show up if I created a new user")
+
+        // window.location.href = "/";
+
+    } catch (err) {
+
+        //what the hell happend
+        console.log("something went south")
+
+    }
+
+}
+
